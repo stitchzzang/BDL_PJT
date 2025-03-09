13b12771e447a61b6c110ec8ca8e9824acd1a89d
woojeonghoon <hoonixox@naver.com>
2025-03-09 17:55:08 +0900

    chore: 절대 경로 설정
    - src를 @로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 });
