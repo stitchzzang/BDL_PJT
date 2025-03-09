21fd4df372d9ebbf2d8023564b8f03eb5d75e82c
woojeonghoon <hoonixox@naver.com>
2025-03-09 18:33:11 +0900

    chore: tailwind color 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#3485FA',
+        'point-color': '#03369D',
+        'background-color': '#041021',
+        'modal-background-color': '#0D192B',
+        'btn-primary-active-color': '#4D4D59',
+        'btn-primary-inactive-color': '#2C2C35',
+        'btn-red-color': '#F23636',
+        'btn-green-color': '#00AC4F',
+        'btn-yellow-color': '#FFBA2F',
+        'btn-blue-color': '#076BFD',
+        'text-main-color': '#FFFFFF',
+        'text-inactive-color': '#E2E8F1',
+        'text-inactive-2-color': '#5E6D84',
+        'border-color': '#718096',
+      },
+    },
   },
   plugins: [],
 };
