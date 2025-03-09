90c1b50032baf7b5f71288ccfc58c52b4c851d32
woojeonghoon <hoonixox@naver.com>
2025-03-09 17:50:14 +0900

    chore: prettier 세팅

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  // 한 줄당 최대 글자 수
+  printWidth: 100,
+  // 객체나 배열의 마지막 요소 뒤에 항상 쉼표 추가
+  trailingComma: 'all',
+  // 들여쓰기 간격 (공백 2칸)
+  tabWidth: 2,
+  // 문장 끝에 세미콜론 추가
+  semi: true,
+  // 문자열에 작은따옴표 사용
+  singleQuote: true,
+  // 객체 리터럴의 중괄호 주위에 공백 추가
+  bracketSpacing: true,
+  // 화살표 함수의 매개변수를 항상 괄호로 감싸기
+  arrowParens: 'always',
+  // 탭 대신 공백 사용
+  useTabs: false,
+  // 줄 끝 문자를 LF(Line Feed)로 통일
+  endOfLine: 'lf',
+};
