bb3e558f59f463ff375205637609e6fd288ebfd1
frndhoon <frndhoon@gmail.com>
2025-03-10 15:37:45 +0900

    refactor: useAuthStore js에서 ts로 변경

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,15 +1,32 @@
 import { jwtDecode } from 'jwt-decode';
 import { create } from 'zustand';
 
-export const useAuthStore = create((set) => ({
+interface UserData {
+  id: number | null;
+  email: string | null;
+  nickname: string | null;
+}
+
+interface AuthState {
+  userData: UserData;
+  isLogin: boolean;
+  isInitialized: boolean;
+  loginAuth: (token: string) => void;
+  logoutAuth: () => void;
+  initializeAuth: () => void;
+}
+
+type JWTPayload = UserData;
+
+export const useAuthStore = create<AuthState>((set) => ({
   // userData만 전역 상태 관리
   userData: { id: null, email: null, nickname: null }, // const {id, email, nickname} = useAuthStore((state) => state.userData) 형식으로 사용
   isLogin: false,
   isInitialized: false,
 
   // 토큰 저장 및 userData 업데이트
-  loginAuth: (token) => {
-    const decoded = jwtDecode(token);
+  loginAuth: (token: string) => {
+    const decoded = jwtDecode<JWTPayload>(token);
     localStorage.setItem('accessToken', token);
     set({ userData: decoded, isLogin: true });
   },
@@ -17,14 +34,14 @@ export const useAuthStore = create((set) => ({
   // 토큰 제거 및 userData 초기화
   logoutAuth: () => {
     localStorage.removeItem('accessToken');
-    set({ userData: {}, isLogin: false });
+    set({ userData: { id: null, email: null, nickname: null }, isLogin: false });
   },
 
   // 토큰 및 userData 초기화 함수 (App.jsx에서 호출 됨에 따라 앱 마운트가 될 때 토큰으로 userData 업데이트)
   initializeAuth: () => {
     const token = localStorage.getItem('accessToken');
     if (token) {
-      const decoded = jwtDecode(token);
+      const decoded = jwtDecode<JWTPayload>(token);
       set({ userData: decoded, isLogin: true, isInitialized: true });
     } else {
       set({ isInitialized: true });
