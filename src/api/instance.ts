7ee6d78038b003f2d22ba5b9f4d1788003663bde
frndhoon <frndhoon@gmail.com>
2025-03-10 15:37:30 +0900

    feat: Ky 인스턴스 생성 및 토큰 관리 로직 구현
    - 기본 및 인증 API 요청을 위한 Ky 인스턴스 생성
    - 액세스 토큰 자동 갱신 및 인증 에러 처리 로직 추가
    - 토큰 만료 시 로그인 페이지로 리다이렉트 기능 구현

---
+++ src/api/instance.ts
@@ -0,0 +1,89 @@
+import ky, { KyRequest } from 'ky';
+
+import { navigate } from '@/lib/navigation';
+import { useAuthStore } from '@/store/useAuthStore';
+import { ERROR_CODES } from '@/utils/errorHandler';
+
+interface ExtendedKyRequest extends KyRequest {
+  _retry?: boolean;
+}
+
+// 기본 ky 인스턴스 생성
+const _ky = ky.create({
+  prefixUrl: import.meta.env.VITE_API_BASE_URL,
+  timeout: 5000,
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  hooks: {
+    beforeError: [
+      (error) => {
+        navigate('/notfound');
+        return error;
+      },
+    ],
+  },
+});
+
+// 인증이 필요한 요청을 위한 ky 인스턴스
+const _kyAuth = _ky.extend({
+  hooks: {
+    beforeRequest: [
+      (request: ExtendedKyRequest) => {
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+          request.headers.set('X-Access-Token', `Bearer ${token}`);
+        }
+      },
+    ],
+    afterResponse: [
+      async (request: ExtendedKyRequest, options, response) => {
+        const clonedResponse = response.clone();
+        const data = await clonedResponse.json();
+        const customCode = data?.body?.code;
+
+        // Access Token 만료
+        if (customCode === ERROR_CODES.EXPIRED_ACCESS_TOKEN) {
+          if (!request._retry) {
+            request._retry = true;
+            try {
+              const tokenResponse = await _ky.post('auth/token');
+              const BEARER_PREFIX = 'Bearer ';
+              const newAccessToken = tokenResponse.headers
+                .get('X-Access-Token')
+                ?.substring(BEARER_PREFIX.length);
+
+              if (newAccessToken) {
+                useAuthStore.getState().loginAuth(newAccessToken);
+                request.headers.set('X-Access-Token', `Bearer ${newAccessToken}`);
+              }
+
+              return _kyAuth(request);
+            } catch (error) {
+              useAuthStore.getState().logoutAuth();
+              navigate('/login');
+              throw error;
+            }
+          }
+        }
+
+        // 모든 토큰 만료 및 유효하지 않은 토큰
+        if (
+          customCode === ERROR_CODES.EXPIRED_ACCESS_TOKEN ||
+          customCode === ERROR_CODES.EXPIRED_REFRESH_TOKEN ||
+          customCode === ERROR_CODES.INVALID_REFRESH_TOKEN ||
+          customCode === ERROR_CODES.NOT_FOUND_REFRESH_TOKEN
+        ) {
+          useAuthStore.getState().logoutAuth();
+          navigate('/login');
+          throw new Error('Authentication failed');
+        }
+
+        return response;
+      },
+    ],
+  },
+});
+
+export { _ky, _kyAuth };
