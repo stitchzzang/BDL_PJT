8b1b859ee60db65602ae4847eceecf15151c8f4a
frndhoon <frndhoon@gmail.com>
2025-03-10 14:07:22 +0900

    feat: react query 세팅 전역적으로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+
+import { queryClient } from '@/lib/queryClient';
 import Router from '@/routes';
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router />
-    </>
+    </QueryClientProvider>
   );
 }
 
