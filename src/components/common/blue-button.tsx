0a4d137a69cd483d6f45d5e1c8ffd85af98536a1
frndhoon <frndhoon@gmail.com>
2025-03-11 17:12:28 +0900

    fix: blue custom button에 onClick 빠진 부분 추가

--- src/components/common/blue-button.tsx
+++ src/components/common/blue-button.tsx
@@ -1,10 +1,17 @@
 import { Button } from '@/components/ui/button';
 
-export const BlueButton = ({ children }: { children: React.ReactNode }) => {
+export const BlueButton = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) => {
   return (
     <Button
       variant="outline"
       className="border-none bg-primary-color text-text-main-color hover:bg-primary-color/20 hover:text-primary-color active:bg-primary-color/20 active:text-primary-color"
+      onClick={onClick}
     >
       {children}
     </Button>
