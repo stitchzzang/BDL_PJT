e4defbadeb65a6051e839e10f4a5a0109f60074b
frndhoon <frndhoon@gmail.com>
2025-03-11 17:13:09 +0900

    feat: navbar navigate 기능 추가

--- src/components/common/nav-bar.tsx
+++ src/components/common/nav-bar.tsx
@@ -1,26 +1,41 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useNavigate } from 'react-router-dom';
 
 import { BlueButton } from '@/components/common/blue-button';
 import { MainLogoIcon } from '@/components/common/icons';
 
 export const NavBar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className="flex items-center justify-between bg-[#030D1B] px-10 py-2">
-      <button className="duration-300 hover:scale-110">
+      <button className="duration-300 hover:scale-110" onClick={() => navigate('/')}>
         <MainLogoIcon className="h-10 w-10" color="white" />
       </button>
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-6">
-          <button className="text-text-inactive-color hover:text-text-main-color active:text-text-main-color">
+          <button
+            className="text-text-inactive-color hover:text-text-main-color active:text-text-main-color"
+            onClick={() => navigate('/')}
+          >
             홈
           </button>
-          <button className="text-text-inactive-color hover:text-text-main-color active:text-text-main-color">
+          <button
+            className="text-text-inactive-color hover:text-text-main-color active:text-text-main-color"
+            onClick={() => navigate('/algorithm-lab')}
+          >
             알고리즘 LAB
           </button>
-          <button className="text-text-inactive-color hover:text-text-main-color active:text-text-main-color">
+          <button
+            className="text-text-inactive-color hover:text-text-main-color active:text-text-main-color"
+            onClick={() => navigate('/simulated-investment')}
+          >
             모의투자
           </button>
-          <button className="text-text-inactive-color hover:text-text-main-color active:text-text-main-color">
+          <button
+            className="text-text-inactive-color hover:text-text-main-color active:text-text-main-color"
+            onClick={() => navigate('/simulated-education')}
+          >
             모의교육
           </button>
         </div>
@@ -37,7 +52,7 @@ export const NavBar = () => {
           />
         </div>
       </div>
-      <BlueButton>로그인</BlueButton>
+      <BlueButton onClick={() => navigate('/login')}>로그인</BlueButton>
     </nav>
   );
 };
