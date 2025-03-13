0b07c27f17cf7e7e40f06e5af68b178a89b3336b
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-13 17:37:59 +0900

    design: 컴포넌트 카테고리 기본 디자인

--- src/components/mockInvestment/orderStatus/order-status-category.tsx
+++ src/components/mockInvestment/orderStatus/order-status-category.tsx
@@ -1,15 +1,16 @@
 export const OrderStatusCategory = () => {
+  const orderButtonStyle = 'w-[30%] cursor-pointer text-center rounded-md py-[10px]';
   return (
     <div className="rounded-md border border-border-color p-3">
-      <div className="flex justify-between">
-        <div>
-          <button>구매</button>
+      <div className="flex w-full justify-between gap-1">
+        <div className={`${orderButtonStyle} bg-btn-red-color bg-opacity-20`}>
+          <button className="font-bold text-btn-red-color">구매</button>
         </div>
-        <div>
-          <button>구매</button>
+        <div className={orderButtonStyle}>
+          <button>판매</button>
         </div>
-        <div>
-          <button>구매</button>
+        <div className={orderButtonStyle}>
+          <button>대기</button>
         </div>
       </div>
     </div>
