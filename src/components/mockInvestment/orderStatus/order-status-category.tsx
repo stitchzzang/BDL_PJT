90f0a119a250769c5dfd40527b545ed614ac45a1
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-13 17:47:20 +0900

    design: 기초 디자인 작업 is active를 통한 스타일 값 변경

--- src/components/mockInvestment/orderStatus/order-status-category.tsx
+++ src/components/mockInvestment/orderStatus/order-status-category.tsx
@@ -1,16 +1,40 @@
+import { useState } from 'react';
+
 export const OrderStatusCategory = () => {
   const orderButtonStyle = 'w-[30%] cursor-pointer text-center rounded-md py-[10px]';
+  const [isActive, setIsActive] = useState<string>('구매');
   return (
     <div className="rounded-md border border-border-color p-3">
       <div className="flex w-full justify-between gap-1">
-        <div className={`${orderButtonStyle} bg-btn-red-color bg-opacity-20`}>
-          <button className="font-bold text-btn-red-color">구매</button>
+        <div
+          className={`${orderButtonStyle} ${isActive === '구매' ? 'bg-btn-red-color bg-opacity-20' : ''}`}
+          onClick={() => setIsActive('구매')}
+        >
+          <button
+            className={`${isActive === '구매' ? 'font-bold text-btn-red-color' : 'font-bold text-border-color'}`}
+          >
+            구매
+          </button>
         </div>
-        <div className={orderButtonStyle}>
-          <button>판매</button>
+        <div
+          className={`${orderButtonStyle} ${isActive === '판매' ? 'bg-btn-blue-color bg-opacity-20' : ''}`}
+          onClick={() => setIsActive('판매')}
+        >
+          <button
+            className={`${isActive === '판매' ? 'font-bold text-btn-blue-color' : 'font-bold text-border-color'}`}
+          >
+            판매
+          </button>
         </div>
-        <div className={orderButtonStyle}>
-          <button>대기</button>
+        <div
+          className={`${orderButtonStyle} ${isActive === '대기' ? 'bg-btn-green-color bg-opacity-20' : ''}`}
+          onClick={() => setIsActive('대기')}
+        >
+          <button
+            className={`${isActive === '대기' ? 'font-bold text-btn-green-color' : 'font-bold text-border-color'}`}
+          >
+            대기
+          </button>
         </div>
       </div>
     </div>
