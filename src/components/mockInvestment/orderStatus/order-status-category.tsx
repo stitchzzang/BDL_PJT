465ff2f128b1e040ec1ed294a46c199d2122e525
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-13 17:15:33 +0900

    feat:order 카테고리 구조화

--- src/components/mockInvestment/orderStatus/order-status-category.tsx
+++ src/components/mockInvestment/orderStatus/order-status-category.tsx
@@ -1,7 +1,11 @@
 export const OrderStatusCategory = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <button>구매</button>
+        <button>판매</button>
+        <button>대기</button>
+      </div>
     </div>
   );
 };
