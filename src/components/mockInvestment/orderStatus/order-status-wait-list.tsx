07fb75947e26c4a951a640a19a60407a66bb0e2c
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-14 17:23:34 +0900

    test:test props 전달 완료

--- src/components/mockInvestment/orderStatus/order-status-wait-list.tsx
+++ src/components/mockInvestment/orderStatus/order-status-wait-list.tsx
@@ -1,3 +1,23 @@
-export const OrderStatusWaitList = () => {
-  return <div></div>;
+import React from 'react';
+
+import { TestItem } from '@/components/mockInvestment/orderStatus/order-status-wait'; // TestItem 타입을 임포트
+
+interface OrderStatusWaitListProps {
+  test: TestItem; // test 객체를 prop으로 받기
+}
+
+export const OrderStatusWaitList: React.FC<OrderStatusWaitListProps> = ({ test }) => {
+  return (
+    <div>
+      <div>
+        <h3>{test.name}</h3>
+        <p>
+          {test.price} | {test.quantity}
+        </p>
+      </div>
+      <div>
+        <p>{test.status}</p>
+      </div>
+    </div>
+  );
 };
--- src/components/mockInvestment/orderStatus/order-status-wait.tsx
+++ src/components/mockInvestment/orderStatus/order-status-wait.tsx
@@ -1,12 +1,25 @@
+import { OrderStatusWaitList } from '@/components/mockInvestment/orderStatus/order-status-wait-list';
+
+// test 객체의 타입 정의
+export interface TestItem {
+  name: string;
+  price: number;
+  quantity: number;
+  status: string;
+}
+
 export const OrderStatusWait = () => {
   const h3Style = 'text-[18px] font-bold text-white';
-  const testList = [
+  const testList: TestItem[] = [
     { name: '삼성전자', price: 12321, quantity: 3, status: '판매' },
     { name: '삼성전자', price: 12321, quantity: 3, status: '판매' },
   ];
   return (
     <div>
       <h3 className={h3Style}>대기 주문</h3>
+      {testList.map((test, index) => (
+        <OrderStatusWaitList test={test} key={index} />
+      ))}
     </div>
   );
 };
