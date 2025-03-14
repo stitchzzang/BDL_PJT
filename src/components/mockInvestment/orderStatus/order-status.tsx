b07dde0ea8525546cb7479d75727fbd46a20ab75
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-14 10:03:44 +0900

    refector: 기존 코드를 세부 컴포넌트로 이동 1. 구매,판매,대기 3가지 컴포넌트로 변경 예정

--- src/components/mockInvestment/orderStatus/order-status.tsx
+++ src/components/mockInvestment/orderStatus/order-status.tsx
@@ -9,46 +9,6 @@ export const OrderStatus = () => {
         </div>
         <hr className="border border-border-color border-opacity-20" />
         <div></div>
-        <h3>구매하기</h3>
-        <div>
-          {/* 구매하기 컴포넌트 -> div 2개로 일단 진행 */}
-          <div>
-            <div>
-              <h3>주문 유형</h3>
-            </div>
-            <div>
-              {/* 지정가 */}
-              <div>
-                <input type="text" />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div>
-              <h3>수량</h3>
-            </div>
-            <div>
-              <div>
-                <input type="text" />
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div>
-            <div>
-              <h3>구매가능 금액</h3>
-              <h3>41,323,323원</h3>
-            </div>
-            <div>
-              <h3>총 주문 금액</h3>
-              <h3>0원</h3>
-            </div>
-          </div>
-          <div>
-            <button>구매하기</button>
-            <p>결제 수수료는 결제 금액의 0.004% 입니다.</p>
-          </div>
-        </div>
       </div>
     </div>
   );
