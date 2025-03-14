182f3399287b53cb75515aa9b3684a089c31d176
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-14 16:43:46 +0900

    feat:카테고리 변경 함수 추가 1. interface 지정후 set함수를 전달 완료,  type지정으로 안정성 추가

--- src/components/mockInvestment/orderStatus/order-status-category.tsx
+++ src/components/mockInvestment/orderStatus/order-status-category.tsx
@@ -1,15 +1,32 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-export const OrderStatusCategory = () => {
+// 허용된 탭 타입을 정의
+type TabType = '구매' | '판매' | '대기';
+
+interface OrderStatusCategoryProps {
+  isActiveCategory: TabType;
+  setIsactiveCategory: React.Dispatch<React.SetStateAction<TabType>>;
+}
+
+export const OrderStatusCategory: React.FC<OrderStatusCategoryProps> = ({
+  isActiveCategory,
+  setIsactiveCategory,
+}) => {
   const orderButtonStyle =
     'w-[30%] cursor-pointer text-center rounded-xl py-2  transition-all duration-300';
   const [isActive, setIsActive] = useState<string>('구매');
+  const changeCategory = (isActiveCategory: TabType) => {
+    setIsactiveCategory(isActiveCategory);
+  };
   return (
     <div className=" rounded-xl border border-border-color p-2 ">
       <div className="flex w-full justify-between gap-1">
         <div
           className={`${orderButtonStyle} ${isActive === '구매' ? 'bg-btn-red-color bg-opacity-20' : ''} hover:bg-btn-red-color hover:bg-opacity-20`}
-          onClick={() => setIsActive('구매')}
+          onClick={() => {
+            setIsActive('구매');
+            changeCategory('구매');
+          }}
         >
           <button
             className={`${isActive === '구매' ? 'font-bold text-btn-red-color' : 'font-bold text-border-color'}`}
@@ -19,7 +36,10 @@ export const OrderStatusCategory = () => {
         </div>
         <div
           className={`${orderButtonStyle} ${isActive === '판매' ? 'bg-btn-blue-color bg-opacity-20' : ''} hover:bg-btn-blue-color hover:bg-opacity-20`}
-          onClick={() => setIsActive('판매')}
+          onClick={() => {
+            setIsActive('판매');
+            changeCategory('판매');
+          }}
         >
           <button
             className={`${isActive === '판매' ? 'font-bold text-btn-blue-color' : 'font-bold text-border-color'}`}
@@ -29,7 +49,10 @@ export const OrderStatusCategory = () => {
         </div>
         <div
           className={`${orderButtonStyle} ${isActive === '대기' ? 'bg-btn-green-color bg-opacity-20' : ''} hover:bg-btn-green-color hover:bg-opacity-20`}
-          onClick={() => setIsActive('대기')}
+          onClick={() => {
+            setIsActive('대기');
+            changeCategory('대기');
+          }}
         >
           <button
             className={`${isActive === '대기' ? 'font-bold text-btn-green-color' : 'font-bold text-border-color'}`}
