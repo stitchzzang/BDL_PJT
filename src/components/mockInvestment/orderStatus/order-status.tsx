6598088f7815bf3b58979f7fba03a431ceb29087
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-14 16:51:09 +0900

    feat:랜더링 구조 추가 1. 구매,판매,대기 패널에 랜더링 조건 구조화

--- src/components/mockInvestment/orderStatus/order-status.tsx
+++ src/components/mockInvestment/orderStatus/order-status.tsx
@@ -1,17 +1,32 @@
+import React, { useState } from 'react';
+
 import { OrderStatusBuy } from '@/components/mockInvestment/orderStatus/order-status-buy';
 import { OrderStatusCategory } from '@/components/mockInvestment/orderStatus/order-status-category';
+import { OrderStatusShell } from '@/components/mockInvestment/orderStatus/order-status-shell';
 
 export const OrderStatus = () => {
+  // 허용된 탭 타입을 정의
+  type TabType = '구매' | '판매' | '대기';
+  // 랜더링 유무
+  const [isActiveCategory, setIsactiveCategory] = useState<TabType>('구매');
+
+  // React.ReactElement 타입 사용
+  const Components: Record<TabType, React.ReactNode> = {
+    구매: <OrderStatusBuy />,
+    판매: <OrderStatusShell />,
+    대기: <OrderStatusBuy />,
+  };
   return (
     <div>
       <div className="rounded-md bg-modal-background-color p-5">
         <div className="mb-[25px]">
-          <OrderStatusCategory />
+          <OrderStatusCategory
+            isActiveCategory={isActiveCategory}
+            setIsactiveCategory={setIsactiveCategory}
+          />
         </div>
         <hr className="mb-[25px] border border-border-color border-opacity-20" />
-        <div>
-          <OrderStatusBuy />
-        </div>
+        <div>{Components[isActiveCategory]}</div>
       </div>
     </div>
   );
