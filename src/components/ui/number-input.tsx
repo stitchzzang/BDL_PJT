8c67589d246f76be9cf34d8812257a9cb4781bc3
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-14 13:57:31 +0900

    feat:숫자 전용 input 추가 숫자 입력시 소수점 포함 정수 제외 어떤 값도 작성하지 않도록 수정

---
+++ src/components/ui/number-input.tsx
@@ -0,0 +1,25 @@
+import { Input } from '@/components/ui/input';
+
+interface InputProps {
+  value: number;
+  setValue: React.Dispatch<React.SetStateAction<number>>;
+  placeholder: string;
+}
+
+export const numberInput: React.FC<InputProps> = ({ value, setValue, placeholder }) => {
+  return (
+    <Input
+      type="text"
+      placeholder={placeholder}
+      value={value === 0 ? '' : setValue.toString()} // 상태값을 string으로 변환해서 value에 전달
+      onChange={(e) => {
+        const value = e.target.value;
+        // 숫자만 허용하는 정규식
+        if (/^\d*$/.test(value)) {
+          const numValue = value === '' ? 0 : parseInt(value, 10); // 빈 값은 0으로 처리
+          setValue(numValue); // number 타입으로 상태를 업데이트
+        }
+      }}
+    />
+  );
+};
