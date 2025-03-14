8deac21ef0f48c9fc2ecf6686dc263ce372a7a69
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-14 17:11:17 +0900

    test:테스트 리스트 추가

--- src/components/mockInvestment/orderStatus/order-status-wait.tsx
+++ src/components/mockInvestment/orderStatus/order-status-wait.tsx
@@ -1,3 +1,12 @@
 export const OrderStatusWait = () => {
-  return <div></div>;
+  const h3Style = 'text-[18px] font-bold text-white';
+  const testList = [
+    { name: '삼성전자', price: 12321, quantity: 3, status: '판매' },
+    { name: '삼성전자', price: 12321, quantity: 3, status: '판매' },
+  ];
+  return (
+    <div>
+      <h3 className={h3Style}>대기 주문</h3>
+    </div>
+  );
 };
