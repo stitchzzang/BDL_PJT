4c177980a17775d7cfbe4dee842787b3524ac7f0
woojeonghoon <hoonixox@naver.com>
2025-03-16 17:46:01 +0900

    remove: order status 컴포넌트 제거 컴포넌트 디자인 확인용이므로 제거

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,10 +1,7 @@
-import { OrderStatus } from '@/components/mockInvestment/orderStatus/order-status';
-
 export const HomePage = () => {
   return (
     <div>
       <div>HomePage</div>
-      <OrderStatus />
     </div>
   );
 };
