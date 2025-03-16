d3f51bc8f5c2f42015992f539d13945e68284ea4
WJG <dnwnsrb11@naver.com>
2025-03-16 16:29:25 +0900

    feat:판매 전체 금액 추가

--- src/components/mockInvestment/orderStatus/order-status-wait.tsx
+++ src/components/mockInvestment/orderStatus/order-status-wait.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { OrderStatusWaitList } from '@/components/mockInvestment/orderStatus/order-status-wait-list';
 
 // test 객체의 타입 정의
@@ -11,16 +13,30 @@ export interface TestItem {
 export const OrderStatusWait = () => {
   const h3Style = 'text-[18px] font-bold text-white';
   const testList: TestItem[] = [
-    { name: '삼성전자', price: 12321, quantity: 3, status: '판매' },
+    { name: '삼성전자', price: 32321, quantity: 3, status: '판매' },
     { name: '삼성전자', price: 12321, quantity: 3, status: '판매' },
     { name: '삼성전자', price: 12321, quantity: 3, status: '구매' },
   ];
+  // 총 판매 금액 표시
+  const [shellCost, setShellCost] = useState<number>(0);
+  // 합산 공식
+  useEffect(() => {
+    const totalCost = testList
+      .filter((item) => item.status === '판매')
+      .reduce((acc, item) => acc + item.price * item.quantity, 0);
+    setShellCost(totalCost);
+  }, [testList]);
   return (
     <div>
       <h3 className={h3Style}>대기 주문</h3>
       {testList.map((test, index) => (
         <OrderStatusWaitList test={test} key={index} />
       ))}
+      <hr className="mb-[20px] mt-[30px] border border-border-color border-opacity-20" />
+      <div className="flex justify-between">
+        <h3 className={h3Style}>총 판매 금액</h3>
+        <h3 className={h3Style}>{shellCost} 원</h3>
+      </div>
     </div>
   );
 };
