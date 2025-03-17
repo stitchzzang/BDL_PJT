3bc144b8e2d6f9c778d34042badfa03f496ed7ca
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 15:14:05 +0900

    feat:기본 스타일링 작업

--- src/components/mock-investment/stock-cost-history/stock-cost-history-day.tsx
+++ src/components/mock-investment/stock-cost-history/stock-cost-history-day.tsx
@@ -1,7 +1,54 @@
-export const StockCostHistoryDay = () => {
+import { DTData } from '@/components/mock-investment/stock-cost-history/stock-cost-history';
+import { formatThousandSeparator } from '@/lib/formatThousandSeparator';
+
+interface StockCostHistoryDayProps {
+  dayDataList: DTData[];
+}
+export const StockCostHistoryDay = ({ dayDataList }: StockCostHistoryDayProps) => {
   return (
     <div>
-      <div></div>
+      <div className="w-full">
+        <div className="flex flex-col space-y-4">
+          <div>{/* 실시간, 일별 */}</div>
+          <div className="flex flex-col space-y-2">
+            {/* 테이블 헤더 */}
+            <div className="rounded-lgp-2 flex flex-row">
+              <div className="w-[20%] text-[16px] text-border-color">일자자</div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">종가</div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">등락률</div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">거래량 (주)</div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">거래대금</div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">시가</div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">고가</div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">저가</div>
+            </div>
+
+            {/* 테이블 로우들 - 배열의 각 항목을 매핑 */}
+            {dayDataList.map((item, index) => (
+              <div key={index} className="flex flex-row rounded-lg bg-[#102038] p-2 text-white">
+                <div className="w-[20%] font-light text-border-color">{item.date}</div>
+                <div className="w-[20%] text-right text-border-color">{item.closePrice}</div>
+                <div className="w-[20%] text-right text-btn-red-color">{item.changeRate}%</div>
+                <div className="w-[20%] text-right font-light text-border-color">
+                  {item.accumulateVolume}
+                </div>
+                <div className="w-[20%] text-right font-light text-border-color">
+                  {formatThousandSeparator(item.accumulatedTradeAmount)}원
+                </div>
+                <div className="w-[20%] text-right font-light text-border-color">
+                  {formatThousandSeparator(item.openPrice)}원
+                </div>
+                <div className="w-[20%] text-right font-light text-border-color">
+                  {item.highPrice} %
+                </div>
+                <div className="w-[20%] text-right font-light text-border-color">
+                  {item.lowPrice}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
