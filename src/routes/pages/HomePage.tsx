d562de7fd97b5b9c476f707941e46f95c9ead332
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 09:49:22 +0900

    test:퍼블리싱 테스트

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import { StockInfo } from '@/components/mock-investment/stock-info/stock-info';
+
 export const HomePage = () => {
   return (
     <div>
       <div>HomePage</div>
+      <StockInfo />
     </div>
   );
 };
