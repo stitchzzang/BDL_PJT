9dbfd4fe9393ca5fb47edf408e154f1b5ed17ab3
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 14:03:10 +0900

    test:테스트 컴포넌트

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,7 +1,12 @@
+import { StockCostHistory } from '@/components/mock-investment/stock-cost-history/stock-cost-history';
+
 export const HomePage = () => {
   return (
     <div>
       <div>HomePage</div>
+      <div>
+        <StockCostHistory />
+      </div>
     </div>
   );
 };
