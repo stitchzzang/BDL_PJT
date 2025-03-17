86e7de30988c87716194bdcf4af5fdf114eb9e79
frndhoon <frndhoon@gmail.com>
2025-03-17 20:38:44 +0900

    design: 회사 선택 버튼 스타일 조정
    - 테두리 라운드 조정
    - padding 조정

--- src/components/common/company-select-button.tsx
+++ src/components/common/company-select-button.tsx
@@ -2,7 +2,7 @@ import { CompanyIdentifier } from '@/components/common/company-identifier';
 
 export const CompanySelectButton = () => {
   return (
-    <button className="min-h-[60px] min-w-[200px] rounded-[10px] border border-btn-primary-inactive-color bg-modal-background-color p-2 hover:bg-transparent active:bg-transparent sm:min-w-[600px]">
+    <button className="min-h-[60px] min-w-[200px] rounded-[20px] border border-btn-primary-inactive-color bg-modal-background-color p-5 hover:bg-transparent active:bg-transparent sm:min-w-[600px]">
       <CompanyIdentifier />
     </button>
   );
