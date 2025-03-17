704fe7e440e3a4f535da0a435ce6db96aec41133
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 10:56:46 +0900

    feat:카테고리 리스트 추가

--- src/components/common/category-button.tsx
+++ src/components/common/category-button.tsx
@@ -1,9 +1,36 @@
-interface categoryData {
+import {
+  CpuChipIcon,
+  CurrencyDollarIcon,
+  EyeDropperIcon,
+  PaintBrushIcon,
+  QueueListIcon,
+  RocketLaunchIcon,
+} from '@heroicons/react/24/outline';
+import { BeakerIcon, CakeIcon, TruckIcon, TvIcon } from 'lucide-react';
+import React from 'react';
+
+export interface categoryData {
   name: string;
-  icon: string;
+  icon: React.ElementType;
 }
 
 export const CategoryButton = () => {
+  const categoryList: categoryData[] = [
+    { name: '자동차', icon: typeof TruckIcon === 'function' ? TruckIcon : () => <></> },
+    { name: '화장품', icon: typeof EyeDropperIcon === 'function' ? EyeDropperIcon : () => <></> },
+    { name: '방위', icon: typeof RocketLaunchIcon === 'function' ? RocketLaunchIcon : () => <></> },
+    { name: '반도체', icon: typeof CpuChipIcon === 'function' ? CpuChipIcon : () => <></> },
+    { name: '바이오', icon: typeof BeakerIcon === 'function' ? BeakerIcon : () => <></> },
+    { name: 'IT', icon: typeof TvIcon === 'function' ? TvIcon : () => <></> },
+    { name: '금속', icon: typeof QueueListIcon === 'function' ? QueueListIcon : () => <></> },
+    { name: '엔터', icon: typeof PaintBrushIcon === 'function' ? PaintBrushIcon : () => <></> },
+    {
+      name: '금융',
+      icon: typeof CurrencyDollarIcon === 'function' ? CurrencyDollarIcon : () => <></>,
+    },
+    { name: '음식', icon: typeof CakeIcon === 'function' ? CakeIcon : () => <></> },
+  ];
+
   return (
     <div>
       <div></div>
