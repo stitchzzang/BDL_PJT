b9ac1f05827a49b7f9eb2fb29f75fc5c4f5ac4f6
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 16:44:05 +0900

    desgin:텍스트 배치

--- src/components/mock-investment/order-status/selling-price/selling-price.tsx
+++ src/components/mock-investment/order-status/selling-price/selling-price.tsx
@@ -7,6 +7,10 @@ export const SellingPrice = () => {
         <p className="text-[14px] font-light text-border-color">12:32</p>
       </div>
       <hr className="border-border-color border-opacity-20" />
+      <div className="flex justify-between">
+        <p className="text-[18px] text-btn-blue-color">매도잔량</p>
+        <p className="text-[18px] text-btn-red-color">매수잔량</p>
+      </div>
       <div>{/* 매도,매수 정리  */}</div>
     </div>
   );
