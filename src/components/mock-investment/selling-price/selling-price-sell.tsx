3b692dac6bd2069d397eef2344ed7baf4003759d
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 16:58:46 +0900

    feat:props 설정

---
+++ src/components/mock-investment/selling-price/selling-price-sell.tsx
@@ -0,0 +1,14 @@
+import { orderBookData } from '@/components/mock-investment/selling-price/selling-price';
+
+interface SellingPriceSellProps {
+  orderBookDummy: orderBookData[];
+}
+
+export const SellingPriceSell = ({ orderBookDummy }: SellingPriceSellProps) => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
