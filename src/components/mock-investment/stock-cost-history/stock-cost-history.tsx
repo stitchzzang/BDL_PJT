a676c10fede9e7434511520a7557be4fb8fc1f2a
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 14:27:14 +0900

    feat:버튼 클릭시 랜더링 유무 체크

--- src/components/mock-investment/stock-cost-history/stock-cost-history.tsx
+++ src/components/mock-investment/stock-cost-history/stock-cost-history.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+
+import { StockCostHistoryDay } from '@/components/mock-investment/stock-cost-history/stock-cost-history-day';
+import { StockCostHistoryRealTime } from '@/components/mock-investment/stock-cost-history/stock-cost-history-realtime';
+
 export const StockCostHistory = () => {
+  const [isActive, setIsActive] = useState<string>('실시간');
   return (
     <div>
-      <div></div>
+      <div>
+        <div>
+          <button onClick={() => setIsActive('실시간')}>실시간</button>
+          <button onClick={() => setIsActive('하루')}>하루</button>
+        </div>
+        <div>{isActive === '실시간' ? <StockCostHistoryRealTime /> : <StockCostHistoryDay />}</div>
+      </div>
     </div>
   );
 };
