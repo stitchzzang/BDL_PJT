7c7866a7ab3d2ca4e7c0f6fad65ce54b734b7ea2
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 09:59:09 +0900

    design: flex 구조화 작업

--- src/components/mock-investment/stock-info/stock-info.tsx
+++ src/components/mock-investment/stock-info/stock-info.tsx
@@ -1,23 +1,24 @@
 import TestImage from '@/assets/test/stock-test.png';
 import { Button } from '@/components/ui/button';
+import { formatThousandSeparator } from '@/lib/formatThousandSeparator';
 
 export const StockInfo = () => {
   return (
-    <div>
-      <div>
-        <div>
+    <div className="flex items-center justify-between border">
+      <div className="flex gap-[20px]">
+        <div className="max-h-[90px] max-w-[90px] overflow-hidden rounded-xl">
           {/* 이미지 */}
           <img src={TestImage} alt="stock-icon" />
         </div>
-        <div>
-          <div>
-            <h3>한화오션</h3>
-            <p>27210</p>
+        <div className="flex flex-col">
+          <div className="flex items-center gap-2">
+            <h3 className="text-[24px] font-medium text-white">한화오션</h3>
+            <p className="text-[16px] font-light text-border-color">27210</p>
           </div>
-          <div>
-            <h3>166,223 원</h3>
-            <div>
-              <div>
+          <div className="flex border">
+            <h3>{formatThousandSeparator(167223)} 원</h3>
+            <div className="flex">
+              <div className="flex">
                 <p>어제보다</p>
                 <p>+1,323원(23%)</p>
               </div>
