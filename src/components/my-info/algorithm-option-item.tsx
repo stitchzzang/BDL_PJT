1f903fcc459f8620fb7f8b81d738c0308f3aeb92
frndhoon <frndhoon@gmail.com>
2025-03-17 14:15:24 +0900

    feat: 알고리즘 옵션 항목 컴포넌트 추가

---
+++ src/components/my-info/algorithm-option-item.tsx
@@ -0,0 +1,8 @@
+export const AlgorithmOptionItem = () => {
+  return (
+    <p className="rounded-[10px] bg-background-color p-2 text-text-border-color">
+      <span>옵션 n : </span>
+      <span>옵션 설명</span>
+    </p>
+  );
+};
