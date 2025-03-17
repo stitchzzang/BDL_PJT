dbeea147f0b2f1d8c3924a26c90c7391f853a084
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 10:36:19 +0900

    remove:테스트 컴포넌트 삭제

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,10 +1,7 @@
-import { StockInfo } from '@/components/mock-investment/stock-info/stock-info';
-
 export const HomePage = () => {
   return (
     <div>
       <div>HomePage</div>
-      <StockInfo />
     </div>
   );
 };
