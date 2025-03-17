7de7fc8b5610ef5d8f1196a0c833920769031848
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 14:50:52 +0900

    test:테스트 props 생성

--- src/components/mock-investment/stock-cost-history/stock-cost-history.tsx
+++ src/components/mock-investment/stock-cost-history/stock-cost-history.tsx
@@ -3,6 +3,34 @@ import { useState } from 'react';
 import { StockCostHistoryDay } from '@/components/mock-investment/stock-cost-history/stock-cost-history-day';
 import { StockCostHistoryRealTime } from '@/components/mock-investment/stock-cost-history/stock-cost-history-realtime';
 
+// 실시간 시세
+export interface realTimeData {
+  tradePrice: number;
+  tradeVolume: number;
+  fluctuationRate: number;
+  tradingVolume: number;
+  tradeTime: string;
+}
+export type RTData = realTimeData;
+
+// 사용 예시
+const stockDataList: RTData[] = [
+  {
+    tradePrice: 50000,
+    tradeVolume: 100,
+    fluctuationRate: 2.5,
+    tradingVolume: 10000,
+    tradeTime: 'YYYY-MM-DD',
+  },
+  {
+    tradePrice: 50000,
+    tradeVolume: 100,
+    fluctuationRate: 2.5,
+    tradingVolume: 10000,
+    tradeTime: 'YYYY-MM-DD',
+  },
+];
+
 export const StockCostHistory = () => {
   const [isActive, setIsActive] = useState<string>('실시간');
   return (
@@ -12,7 +40,13 @@ export const StockCostHistory = () => {
           <button onClick={() => setIsActive('실시간')}>실시간</button>
           <button onClick={() => setIsActive('하루')}>하루</button>
         </div>
-        <div>{isActive === '실시간' ? <StockCostHistoryRealTime /> : <StockCostHistoryDay />}</div>
+        <div>
+          {isActive === '실시간' ? (
+            <StockCostHistoryRealTime stockDataList={stockDataList} />
+          ) : (
+            <StockCostHistoryDay />
+          )}
+        </div>
       </div>
     </div>
   );
