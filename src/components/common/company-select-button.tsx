71d33d88d4f75b4ece9fd9e9f332d882f19dc0b3
frndhoon <frndhoon@gmail.com>
2025-03-17 15:20:04 +0900

    feat: 회사 선택 버튼 컴포넌트 추가

---
+++ src/components/common/company-select-button.tsx
@@ -0,0 +1,7 @@
+export const CompanySelectButton = () => {
+  return (
+    <button className="min-h-[60px] min-w-[200px] rounded-[10px] border border-btn-primary-inactive-color bg-modal-background-color p-2 hover:bg-transparent active:bg-transparent sm:min-w-[600px]">
+      <p>주식</p>
+    </button>
+  );
+};
