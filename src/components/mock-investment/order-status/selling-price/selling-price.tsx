977ee8b277c07fccefbfe2834dd398e16da80b3e
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 16:28:47 +0900

    feat:호가 컴포넌트 기초 작업

--- src/components/mock-investment/order-status/selling-price/selling-price.tsx
+++ src/components/mock-investment/order-status/selling-price/selling-price.tsx
@@ -1,7 +1,13 @@
 export const SellingPrice = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <h3>호가</h3>
+        {/* 장시간 string */}
+        <p>12:32</p>
+      </div>
+      <hr />
+      <div>{/* 매도,매수 정리  */}</div>
     </div>
   );
 };
