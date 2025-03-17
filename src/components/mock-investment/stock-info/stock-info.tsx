e84cf17df0582c4252a974211c279b2ddd64d2c7
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 09:45:14 +0900

    feat:기본 구조 생성

--- src/components/mock-investment/stock-info/stock-info.tsx
+++ src/components/mock-investment/stock-info/stock-info.tsx
@@ -1,7 +1,24 @@
+import { Button } from '@/components/ui/button';
+
 export const StockInfo = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <div>
+          {/* 이미지 */}
+        </div>
+        <div>
+          <div>
+            {/* 제목 */}
+          </div>
+          <div>
+            {/* 하단 정보 */}
+          </div>
+        </div>
+      </div>
+      <div>
+        <Button>알고리즘 선택</Button>
+      </div>
     </div>
   );
 };
