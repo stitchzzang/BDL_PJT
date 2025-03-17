ad4b9d6f4100e6ceb3049bf5bf7b96891953ba9c
frndhoon <frndhoon@gmail.com>
2025-03-17 14:54:04 +0900

    feat: shadcn/ui badge 컴포넌트 variants 추가 - main - increase(수익 증가) - decrease(수익 감소)

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-md p-3 text-base transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
@@ -14,6 +14,9 @@ const badgeVariants = cva(
         destructive:
           'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
         outline: 'text-foreground',
+        main: 'bg-modal-background-color text-primary-foreground',
+        increase: 'bg-btn-blue-color/20',
+        decrease: 'bg-btn-red-color/20',
       },
     },
     defaultVariants: {
