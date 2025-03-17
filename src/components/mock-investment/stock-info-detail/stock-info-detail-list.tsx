b521a5edb6dcb072d11f9313c08a1070a439db5a
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 16:12:49 +0900

    desgin:기본 스타일링 작업-리스트

--- src/components/mock-investment/stock-info-detail/stock-info-detail-list.tsx
+++ src/components/mock-investment/stock-info-detail/stock-info-detail-list.tsx
@@ -1,21 +1,56 @@
 import { stockInfoDetail } from '@/components/mock-investment/stock-info-detail/stock-info-detail';
+import { formatThousandSeparator } from '@/lib/formatThousandSeparator';
 
 interface StockInfoDetailListProps {
   data: stockInfoDetail; // 타입 이름을 맞춰야 함
 }
 
 export const StockInfoDetailList = ({ data }: StockInfoDetailListProps) => {
+  const divStyle = 'flex justify-between rounded-2xl bg-[#102038] px-[20px] py-[20px]';
   return (
-    <div>
-      <div>자본금: {data.capital.toLocaleString()} 원</div>
-      <div>상장주식수: {data.listedSharesCnt.toLocaleString()} 주</div>
-      <div>상장시가총액: {data.listedCapitalAmount.toLocaleString()} 원</div>
-      <div>액면가: {data.parValue.toLocaleString()} 원</div>
-      <div>발행가: {data.issuePrice.toLocaleString()} 원</div>
-      <div>종가: {data.closePrice.toLocaleString()} 원</div>
-      <div>전일종가: {data.previousClosePrice.toLocaleString()} 원</div>
-      <div>거래정지 여부: {data.isTradingStop ? '예' : '아니오'}</div>
-      <div>관리종목 여부: {data.isAdministrationItem ? '예' : '아니오'}</div>
+    <div className="flex flex-col gap-[20px]">
+      <div className={divStyle}>
+        <span className="text-[18px] text-border-color">자본금:</span>
+        <span className="text-[24px] font-bold">{formatThousandSeparator(data.capital)} 원</span>
+      </div>
+      <div className={divStyle}>
+        <span className="text-[18px] text-border-color">상장주식수:</span>
+        <span className="text-[24px] font-bold">
+          {formatThousandSeparator(data.listedSharesCnt)} 원
+        </span>
+      </div>
+      <div className={divStyle}>
+        <span className="text-[18px] text-border-color">상장시가총액:</span>
+        <span className="text-[24px] font-bold">
+          {formatThousandSeparator(data.listedCapitalAmount)} 원
+        </span>
+      </div>
+      <div className={divStyle}>
+        <span className="text-[18px] text-border-color">액면가:</span>
+        <span className="text-[24px] font-bold">{formatThousandSeparator(data.parValue)} 원</span>
+      </div>
+      <div className={divStyle}>
+        <span className="text-[18px] text-border-color">발행가:</span>
+        <span className="text-[24px] font-bold">{formatThousandSeparator(data.issuePrice)} 원</span>
+      </div>
+      <div className={divStyle}>
+        <span className="text-[18px] text-border-color">종가:</span>
+        <span className="text-[24px] font-bold">{formatThousandSeparator(data.closePrice)} 원</span>
+      </div>
+      <div className={divStyle}>
+        <span className="text-[18px] text-border-color">전일종가:</span>
+        <span className="text-[24px] font-bold">
+          {formatThousandSeparator(data.previousClosePrice)} 원
+        </span>
+      </div>
+      <div className={divStyle}>
+        <span className="text-[18px] text-border-color">거래정지 여부:</span>
+        <span className="text-[24px] font-bold">{data.isTradingStop ? '예' : '아니오'}</span>
+      </div>
+      <div className={divStyle}>
+        <span className="text-[18px] text-border-color">관리종목 여부:</span>
+        <span className="text-[24px] font-bold">{data.isAdministrationItem ? '예' : '아니오'}</span>
+      </div>
     </div>
   );
 };
