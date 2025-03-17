2f8e724db74ec000bc8d8f33f33f2b466dad0b77
frndhoon <frndhoon@gmail.com>
2025-03-17 20:37:37 +0900

    feat: 회사 선택 버튼에 회사 로고 및 이름 컴포넌트 추가

--- src/components/common/company-select-button.tsx
+++ src/components/common/company-select-button.tsx
@@ -1,7 +1,9 @@
+import { CompanyIdentifier } from '@/components/common/company-identifier';
+
 export const CompanySelectButton = () => {
   return (
     <button className="min-h-[60px] min-w-[200px] rounded-[10px] border border-btn-primary-inactive-color bg-modal-background-color p-2 hover:bg-transparent active:bg-transparent sm:min-w-[600px]">
-      <p>주식</p>
+      <CompanyIdentifier />
     </button>
   );
 };
