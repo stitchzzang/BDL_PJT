4813cc41ddca0d03d2ef32522132dbde85d47b88
frndhoon <frndhoon@gmail.com>
2025-03-17 14:19:59 +0900

    style: prettier 코드 포매팅 적용

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 
 // ✅ cva를 활용하여 input 스타일 정의
 const inputVariants = cva(
-  'flex w-full rounded-md border border-border-color bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+  'border-border-color flex w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
   {
     variants: {
       variant: {
