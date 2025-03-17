78d83ac20b1ab6493ca9d45607cf8ec496212339
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 12:25:26 +0900

    feat:카테고리 전체 구조화 완료 - 디자인 작업 진행

--- src/components/common/category-button.tsx
+++ src/components/common/category-button.tsx
@@ -11,29 +11,36 @@ import React from 'react';
 
 export interface categoryData {
   name: string;
-  icon: React.ElementType;
+  icon: React.ComponentType; // ComponentType으로 변경
 }
 
 export const CategoryButton = () => {
   const categoryList: categoryData[] = [
-    { name: '자동차', icon: typeof TruckIcon === 'function' ? TruckIcon : () => <></> },
-    { name: '화장품', icon: typeof EyeDropperIcon === 'function' ? EyeDropperIcon : () => <></> },
-    { name: '방위', icon: typeof RocketLaunchIcon === 'function' ? RocketLaunchIcon : () => <></> },
-    { name: '반도체', icon: typeof CpuChipIcon === 'function' ? CpuChipIcon : () => <></> },
-    { name: '바이오', icon: typeof BeakerIcon === 'function' ? BeakerIcon : () => <></> },
-    { name: 'IT', icon: typeof TvIcon === 'function' ? TvIcon : () => <></> },
-    { name: '금속', icon: typeof QueueListIcon === 'function' ? QueueListIcon : () => <></> },
-    { name: '엔터', icon: typeof PaintBrushIcon === 'function' ? PaintBrushIcon : () => <></> },
-    {
-      name: '금융',
-      icon: typeof CurrencyDollarIcon === 'function' ? CurrencyDollarIcon : () => <></>,
-    },
-    { name: '음식', icon: typeof CakeIcon === 'function' ? CakeIcon : () => <></> },
+    { name: '자동차', icon: TruckIcon },
+    { name: '화장품', icon: EyeDropperIcon },
+    { name: '방위', icon: RocketLaunchIcon },
+    { name: '반도체', icon: CpuChipIcon },
+    { name: '바이오', icon: BeakerIcon },
+    { name: 'IT', icon: TvIcon },
+    { name: '금속', icon: QueueListIcon },
+    { name: '엔터', icon: PaintBrushIcon },
+    { name: '금융', icon: CurrencyDollarIcon },
+    { name: '음식', icon: CakeIcon },
   ];
 
   return (
     <div>
-      <div></div>
+      <div>
+        {categoryList.map((data, index) => {
+          const IconComponent = data.icon;
+          return (
+            <div key={index}>
+              <IconComponent /> {/* 이제 정상적으로 렌더링됨 */}
+              {data.name}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
