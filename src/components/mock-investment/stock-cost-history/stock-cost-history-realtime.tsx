af0344d80a88769c954956d1c992843a9bc1465d
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 14:51:14 +0900

    feat:구조화 작업

--- src/components/mock-investment/stock-cost-history/stock-cost-history-realtime.tsx
+++ src/components/mock-investment/stock-cost-history/stock-cost-history-realtime.tsx
@@ -1,4 +1,9 @@
-export const StockCostHistoryRealTime = () => {
+import { RTData } from '@/components/mock-investment/stock-cost-history/stock-cost-history';
+
+interface StockCostHistoryRealTimeProps {
+  stockDataList: RTData[];
+}
+export const StockCostHistoryRealTime = ({ stockDataList }: StockCostHistoryRealTimeProps) => {
   return (
     <div>
       <div className="w-full">
@@ -8,20 +13,26 @@ export const StockCostHistoryRealTime = () => {
             {/* 테이블 헤더 */}
             <div className="rounded-lgp-2 flex flex-row">
               <div className="w-[20%] text-[16px] text-border-color">채결가</div>
-              <div className="w-[20%] text-[16px] text-border-color">체결량(주)</div>
-              <div className="w-[20%] text-[16px] text-border-color">등락률률</div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">체결량(주)</div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">등락률률</div>
               <div className="w-[20%] text-right text-[16px] text-border-color">거래량 (주)</div>
               <div className="w-[20%] text-right text-[16px] text-border-color">시간</div>
             </div>
 
-            {/* 테이블 로우들 */}
-            <div className="flex flex-row rounded-lg bg-[#102038] p-2 text-white">
-              <div className="w-[20%] font-medium">INV001</div>
-              <div className="w-[20%]">Paid</div>
-              <div className="w-[20%]">Credit Card</div>
-              <div className="w-[20%] text-right">$250.00</div>
-              <div className="w-[20%] text-right">13:32:23</div>
-            </div>
+            {/* 테이블 로우들 - 배열의 각 항목을 매핑 */}
+            {stockDataList.map((item, index) => (
+              <div key={index} className="flex flex-row rounded-lg bg-[#102038] p-2 text-white">
+                <div className="w-[20%] font-medium">{item.tradePrice}원</div>
+                <div className="w-[20%] text-right text-btn-blue-color">{item.tradeVolume}</div>
+                <div className="w-[20%] text-right text-btn-red-color">{item.fluctuationRate}%</div>
+                <div className="w-[20%] text-right font-light text-border-color">
+                  {item.tradingVolume}
+                </div>
+                <div className="w-[20%] text-right font-light text-border-color">
+                  {item.tradeTime}
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
