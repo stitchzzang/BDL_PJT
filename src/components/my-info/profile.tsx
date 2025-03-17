8ae02469a81e1b474325e5411d06a93102b55e02
frndhoon <frndhoon@gmail.com>
2025-03-17 12:58:27 +0900

    feat: 마이페이지 profile 섹션 컴포넌트 생성

---
+++ src/components/my-info/profile.tsx
@@ -0,0 +1,14 @@
+import { Cog6ToothIcon } from '@heroicons/react/24/outline';
+
+export const Profile = () => {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <img src="https://placehold.co/80x80" alt="profile" className="h-10 w-10 rounded-full" />
+      <p className="text-2xl font-medium">홍길동</p>
+      <button className="flex items-center gap-1 rounded-lg px-4 py-2 text-text-border-color hover:bg-modal-background-color hover:text-text-main-color active:bg-modal-background-color active:text-text-main-color">
+        <Cog6ToothIcon className="h-5 w-5" />
+        <p className="text-sm">프로필 수정</p>
+      </button>
+    </div>
+  );
+};
