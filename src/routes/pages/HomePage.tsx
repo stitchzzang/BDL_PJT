37a1d376f8e092a957d2c5597a9d6cd7a1b85ae4
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 15:51:17 +0900

    test:테스트 컴포넌트 생성

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import { StockInfoDetail } from '@/components/mock-investment/stock-info-detail/stock-info-detail';
+
 export const HomePage = () => {
   return (
     <div>
       <div>HomePage</div>
+      <StockInfoDetail />
     </div>
   );
 };
