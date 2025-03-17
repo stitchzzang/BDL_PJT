19e64534ef5afd47b83ec73f87d4bbc74c11b263
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 16:19:45 +0900

    test: 메인페이지에 테스트 컴포넌트 생성

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import { SellingPrice } from '@/components/mock-investment/order-status/selling-price/selling-price';
+
 export const HomePage = () => {
   return (
     <div>
       <div>HomePage</div>
+      <SellingPrice />
     </div>
   );
 };
