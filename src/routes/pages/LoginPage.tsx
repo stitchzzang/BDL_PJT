04772b8799aa41743554227d078677ba09b62d05
frndhoon <frndhoon@gmail.com>
2025-03-17 17:20:36 +0900

    feat: login page 퍼블리싱 - input component 활용 - button component 활용 - 비밀번호/회원가입 찾기 같은 단순 버튼 컴포넌트 재사용 여부 확인 필요

--- src/routes/pages/LoginPage.tsx
+++ src/routes/pages/LoginPage.tsx
@@ -1,3 +1,31 @@
+import { MainLogoIcon } from '@/components/common/icons';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <MainLogoIcon color="white" className="h-36 w-36" />
+      <div className="w-full max-w-96 rounded-lg bg-modal-background-color p-6 shadow-lg">
+        <div className="flex flex-col items-center justify-center gap-3">
+          <Input placeholder="이메일" className="h-12" />
+          <Input placeholder="비밀번호" className="h-12" />
+          <Button variant="blue" className="mt-5 w-full">
+            로그인
+          </Button>
+          <div className="flex w-full flex-row items-end justify-end gap-5">
+            <button className="text-base text-border-color hover:text-primary-color">
+              비밀번호 찾기
+            </button>
+            <button className="text-base text-border-color hover:text-primary-color">
+              회원가입
+            </button>
+          </div>
+        </div>
+      </div>
+      <p className="mt-5 text-center text-sm text-border-color">
+        © 2025 B.LAB. All rights reserved.
+      </p>
+    </div>
+  );
 };
