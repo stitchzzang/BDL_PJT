2d8957ee50dc83c71b268cfaba1c929f1231dbb5
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 12:57:45 +0900

    test:테스트 컴포넌트 삭제

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,10 +1,7 @@
-import { CategoryButton } from '@/components/common/category-button';
-
 export const HomePage = () => {
   return (
     <div>
       <div>HomePage</div>
-      <CategoryButton />
     </div>
   );
 };
