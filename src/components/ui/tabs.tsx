56e7dfee56e2fa9a91c20a36fe0e34bf209b17d9
frndhoon <frndhoon@gmail.com>
2025-03-17 15:58:50 +0900

    design: 탭 컴포넌트의 배경색 및 패딩 추가, 텍스트 색상 변경

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -41,7 +41,7 @@ export const Tabs = ({
     <>
       <div
         className={cn(
-          'flex flex-row items-center justify-start [perspective:1000px] relative overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full w-full',
+          'flex flex-row items-center justify-start [perspective:1000px] relative overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full w-full bg-modal-background-color p-3 rounded-2xl',
           containerClassName,
         )}
       >
@@ -63,13 +63,13 @@ export const Tabs = ({
                 layoutId="clickedbutton"
                 transition={{ type: 'spring', bounce: 0.3, duration: 0.6 }}
                 className={cn(
-                  'absolute inset-0 bg-gray-200 dark:bg-zinc-800 rounded-full ',
+                  'absolute inset-0 bg-gray-200 dark:bg-zinc-800 rounded-xl ',
                   activeTabClassName,
                 )}
               />
             )}
 
-            <span className="relative block text-black dark:text-white">{tab.title}</span>
+            <span className="relative block text-text-main-color">{tab.title}</span>
           </button>
         ))}
       </div>
