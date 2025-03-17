b9a73a0823b8d68dea5d34648fbfaab6c87f8fb2
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 17:19:46 +0900

    rename:테스트 내용 삭제

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,12 +1,7 @@
-import { StockCostHistory } from '@/components/mock-investment/stock-cost-history/stock-cost-history';
-
 export const HomePage = () => {
   return (
     <div>
       <div>HomePage</div>
-      <div>
-        <StockCostHistory />
-      </div>
     </div>
   );
 };
