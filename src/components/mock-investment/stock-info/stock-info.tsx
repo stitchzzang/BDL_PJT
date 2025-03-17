695aae2fca57184b74aa320027d5147d0eecc82d
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-17 10:05:00 +0900

    design:기초 디자인 작업 완료

--- src/components/mock-investment/stock-info/stock-info.tsx
+++ src/components/mock-investment/stock-info/stock-info.tsx
@@ -4,8 +4,8 @@ import { formatThousandSeparator } from '@/lib/formatThousandSeparator';
 
 export const StockInfo = () => {
   return (
-    <div className="flex items-center justify-between border">
-      <div className="flex gap-[20px]">
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-[20px]">
         <div className="max-h-[90px] max-w-[90px] overflow-hidden rounded-xl">
           {/* 이미지 */}
           <img src={TestImage} alt="stock-icon" />
@@ -15,15 +15,17 @@ export const StockInfo = () => {
             <h3 className="text-[24px] font-medium text-white">한화오션</h3>
             <p className="text-[16px] font-light text-border-color">27210</p>
           </div>
-          <div className="flex border">
-            <h3>{formatThousandSeparator(167223)} 원</h3>
-            <div className="flex">
-              <div className="flex">
-                <p>어제보다</p>
-                <p>+1,323원(23%)</p>
+          <div className="flex gap-[18px]">
+            <h3 className="text-[30px] font-medium text-white">
+              {formatThousandSeparator(167223)}원
+            </h3>
+            <div className="flex gap-[18px]">
+              <div className="flex gap-[15px] rounded-lg bg-modal-background-color px-[15px] py-[10px]">
+                <p className="text-border-color">어제보다</p>
+                <p className="text-btn-red-color">{formatThousandSeparator(1323)}(23%)</p>
               </div>
-              <div>
-                <p>반도체체</p>
+              <div className="flex gap-[15px] rounded-lg bg-modal-background-color px-[15px] py-[10px]">
+                <p className="text-border-color">반도체체</p>
               </div>
             </div>
           </div>
