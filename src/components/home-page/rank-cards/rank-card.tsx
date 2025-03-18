927926bbaeb9e1df5066141e5319d7100bfb2673
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-18 14:04:16 +0900

    test: 더미데이터 추가

--- src/components/home-page/rank-cards/rank-card.tsx
+++ src/components/home-page/rank-cards/rank-card.tsx
@@ -1,7 +1,18 @@
-export const RankCard = () => {
+import { RankInformation } from '@/components/home-page/rank-cards/rank-cards';
+
+interface RankCardProps {
+  rankInfo: RankInformation;
+}
+
+export const RankCard = ({ rankInfo }: RankCardProps) => {
+  const { name, rate, imgUrl } = rankInfo;
   return (
     <div>
-      <div></div>
+      <div>
+        <p>{name}</p>
+        <p>{rate}</p>
+        <p>{imgUrl}</p>
+      </div>
     </div>
   );
 };
--- src/components/home-page/rank-cards/rank-cards.tsx
+++ src/components/home-page/rank-cards/rank-cards.tsx
@@ -1,10 +1,38 @@
 import { RankCard } from '@/components/home-page/rank-cards/rank-card';
 
+export interface RankInformation {
+  name: string;
+  rate: number;
+  imgUrl: string | null;
+}
+
+const RankInformations: RankInformation[] = [
+  {
+    name: '크와와왕',
+    rate: 23.2,
+    imgUrl: null,
+  },
+  {
+    name: '크와와왕',
+    rate: 23.2,
+    imgUrl: null,
+  },
+  {
+    name: '크와와왕',
+    rate: 23.2,
+    imgUrl: null,
+  },
+];
+
 export const RankCards = () => {
   return (
     <div>
       <div>
-        <RankCard />
+        {RankInformations.map((rankInfo, index) => (
+          <div>
+            <RankCard />
+          </div>
+        ))}
       </div>
     </div>
   );
