2f908d944aa7aedbc95eb98e96c22e7ce81560f0
frndhoon <frndhoon@gmail.com>
2025-03-18 15:15:36 +0900

    refactor: 파일명 대소문자 수정 (memberEditProfile -> MemberEditProfile)

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,7 +5,7 @@ import { MemberLayout } from '@/layouts/MemberLayout';
 import { AlgorithmLabPage } from '@/routes/pages/AlgorithmLabPage';
 import { HomePage } from '@/routes/pages/HomePage';
 import { LoginPage } from '@/routes/pages/LoginPage';
-import { MemberEditProfile } from '@/routes/pages/memberEditProfile';
+import { MemberEditProfile } from '@/routes/pages/MemberEditProfile';
 import { NotFoundPage } from '@/routes/pages/NotFoundPage';
 import { SearchPage } from '@/routes/pages/SearchPage';
 import { SimulatedEducationPage } from '@/routes/pages/SimulatedEducationPage';
