e439605f187efeeb9fda01ef8e7eec71d4015a45
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-18 10:28:05 +0900

    feat:Record를 사용하여 컴포넌트 매핑

--- src/components/home-page/real-time-chart/real-time-chart.tsx
+++ src/components/home-page/real-time-chart/real-time-chart.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 // 차트 타입 정의
 enum chartType {
@@ -9,6 +9,13 @@ enum chartType {
 
 export const RealTimeChart = () => {
   const [isActive, setIsActive] = useState<string>('거래대금');
+
+  // Record를 사용하여 컴포넌트 매핑
+  const components : Record<chartType, React.ReactNode> = {
+    [chartType.TRADING_VOLUME]:
+    [chartType.RAPID_RISE]:
+    [chartType.RAPID_FALL]:
+  }
   return (
     <div>
       <div></div>
