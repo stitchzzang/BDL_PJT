01613220323888db8d792e36d56ced36d6d67bd4
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-18 13:25:15 +0900

    desgin:리스트 디자인

--- src/components/home-page/real-time-chart/real-time-chart-fall.tsx
+++ src/components/home-page/real-time-chart/real-time-chart-fall.tsx
@@ -1,3 +1,6 @@
+import noneStockImg from '@/assets/none-img/none_stock_img.png';
+import { formatThousandSeparator } from '@/lib/formatThousandSeparator';
+
 export interface StockFall {
   stockName: string;
   currentPrice: number;
@@ -25,7 +28,45 @@ const StockFalls: StockFalls[] = [
 export const RealTimeChartFall = () => {
   return (
     <div>
-      <div></div>
+      <div className="w-full">
+        <div className="!mt-0 flex flex-col">
+          <div>{/* 실시간, 일별 */}</div>
+          <div className="flex flex-col space-y-2">
+            {/* 테이블 헤더 */}
+            <div className="rounded-lgp-2 flex flex-row">
+              <div className="w-[60%] text-[14px] text-border-color">종목</div>
+              <div className="w-[20%] text-right text-[14px] text-border-color">현재가</div>
+              <div className="w-[20%] text-right text-[14px] text-btn-blue-color">
+                등락률 낮은 순
+              </div>
+            </div>
+
+            {/* 테이블 로우들 - 배열의 각 항목을 매핑 */}
+            {StockFalls.map((stockFall, index) => (
+              <div
+                key={index}
+                className="flex flex-row items-center rounded-lg bg-[#102038] p-3 text-white hover:bg-modal-background-color"
+              >
+                <div className="flex w-[60%] items-center gap-3 font-medium">
+                  <h3 className="text-[20px] font-bold">{index + 1}</h3>
+                  <div className="max-h-[50px] max-w-[50px] overflow-hidden rounded-xl">
+                    {stockFall.imageUrl === null ? (
+                      <img src={noneStockImg} alt="noneimage" />
+                    ) : (
+                      <img src={stockFall.imageUrl} alt="stockprofileimage" />
+                    )}
+                  </div>
+                  <h3 className="text-[16px] font-medium">{stockFall.stockName}</h3>
+                </div>
+                <div className="w-[20%] text-right">
+                  {formatThousandSeparator(stockFall.currentPrice)} 원
+                </div>
+                <div className="w-[20%] text-right text-btn-red-color">{stockFall.changeRate}%</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
