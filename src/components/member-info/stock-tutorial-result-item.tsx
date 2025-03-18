83bab2111d43d1f93c978d884a09dd9f07b36f25
frndhoon <frndhoon@gmail.com>
2025-03-18 16:30:25 +0900

    design: 프로토타입에 맞게 border-color로 수정

--- src/components/member-info/stock-tutorial-result-item.tsx
+++ src/components/member-info/stock-tutorial-result-item.tsx
@@ -2,19 +2,20 @@ import { CompanyInfo } from '@/components/common/company-info';
 
 export const StockTutorialResultItem = () => {
   return (
-    <div className="flex flex-row items-center justify-between gap-2 rounded-[10px] bg-modal-background-color p-3">
+    <div className="flex w-full flex-row items-center justify-between gap-2 rounded-[10px] bg-modal-background-color p-3">
       <CompanyInfo />
       <div className="flex flex-row items-center gap-4 text-base">
         <div className="flex flex-row items-center gap-1">
-          <span className="text-text-border-color">시작 금액</span>
+          <span className="text-border-color">시작 금액</span>
           <p className="text-text-main-color">100,000원</p>
         </div>
         <div className="flex flex-row items-center gap-1">
-          <span className="text-text-border-color">최종 금액</span>
-          <p className="text-text-main-color">100,000원</p>
+          <span className="text-border-color">최종 금액</span>
+          <p className="text-text-main-color">100,000</p>
+          <span className="text-border-color">원</span>
         </div>
         <div className="flex flex-row items-center gap-1">
-          <span className="text-text-border-color">최종 수익률</span>
+          <span className="text-border-color">최종 수익률</span>
           <p className="rounded-lg border border-btn-red-color px-2 py-1 text-btn-red-color">
             +34.2%
           </p>
