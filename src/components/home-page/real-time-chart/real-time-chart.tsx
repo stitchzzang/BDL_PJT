22072c0750e032d7b78efe02452d130ba03711de
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-18 10:25:46 +0900

    feat:enum 차트 타입 정의

--- src/components/home-page/real-time-chart/real-time-chart.tsx
+++ src/components/home-page/real-time-chart/real-time-chart.tsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
+// 차트 타입 정의
+enum chartType {
+  TRADING_VOLUME = '거래대금',
+  RAPID_RISE = '급상승',
+  RAPID_FALL = '급하락',
+}
+
 export const RealTimeChart = () => {
+  const [isActive, setIsActive] = useState<string>('거래대금');
   return (
     <div>
-      <div> </div>
+      <div></div>
     </div>
   );
 };
