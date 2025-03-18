8d36ee8f18560f24ddf691ef7895c5e53d910755
frndhoon <frndhoon@gmail.com>
2025-03-18 15:13:46 +0900

    design: member layout 수정 - 중앙 정렬 및 margin 추가

--- src/layouts/MemberLayout.tsx
+++ src/layouts/MemberLayout.tsx
@@ -3,10 +3,12 @@ import { SectionNavBar } from '@/components/member-info/section-nav-bar';
 
 export const MemberLayout = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <Profile />
-      <SectionNavBar />
-      <hr className="border-t border-btn-primary-inactive-color" />
-    </div>
+    <>
+      <div className="flex flex-col items-center gap-4">
+        <Profile />
+        <SectionNavBar />
+      </div>
+      <hr className="mb-5 mt-3 border-t border-btn-primary-inactive-color" />
+    </>
   );
 };
