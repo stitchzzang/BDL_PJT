a32ef241d78b897cac31090de53232c50109b957
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-18 13:14:08 +0900

    test:더미데이터 추가

--- src/components/home-page/real-time-chart/real-time-chart-rise.tsx
+++ src/components/home-page/real-time-chart/real-time-chart-rise.tsx
@@ -1,3 +1,27 @@
+export interface StockRise {
+  stockName: string;
+  currentPrice: number;
+  changeRate: number;
+  imageUrl: string | null;
+}
+
+export type StockRises = StockRise;
+
+const StockRises: StockRises[] = [
+  {
+    stockName: '삼성전자',
+    currentPrice: 140203010,
+    changeRate: 3.15,
+    imageUrl: null,
+  },
+  {
+    stockName: '삼성전자',
+    currentPrice: 140203010,
+    changeRate: 3.15,
+    imageUrl: null,
+  },
+];
+
 export const RealTimeChartRise = () => {
   return (
     <div>
