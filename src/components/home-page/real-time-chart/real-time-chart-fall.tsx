fafdd54b28ebc7ecc81928f04045d6289888d10f
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-18 10:29:18 +0900

    feat:급상승, 급하락 컴포넌트 생성

---
+++ src/components/home-page/real-time-chart/real-time-chart-fall.tsx
@@ -0,0 +1,7 @@
+export const RealTimeChartFall = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
---
+++ src/components/home-page/real-time-chart/real-time-chart-rise.tsx
@@ -0,0 +1,7 @@
+export const RealTimeChartRise = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
