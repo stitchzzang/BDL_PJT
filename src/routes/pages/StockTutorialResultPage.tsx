bfa2ee500932a3ae1bc8913ec612be4d3a62b876
frndhoon <frndhoon@gmail.com>
2025-03-18 16:34:12 +0900

    feat: 주식 튜토리얼 결과 페이지 퍼블리싱

--- src/routes/pages/StockTutorialResultPage.tsx
+++ src/routes/pages/StockTutorialResultPage.tsx
@@ -2,8 +2,12 @@ import { StockTutorialResultItem } from '@/components/member-info/stock-tutorial
 
 export const StockTutorialResultPage = () => {
   return (
-    <div className="flex flex-col items-center gap-4">
-      <h1 className="text-2xl font-bold">주식 튜토리얼 결과</h1>
+    <div className="mx-auto flex w-full max-w-[1000px] flex-col items-center gap-4">
+      <div className="flex w-full flex-row justify-between">
+        <h1 className="text-2xl font-bold">주식 튜토리얼 결과</h1>
+        <p className="text-text-inactive-2-color">{new Date().toISOString().split('T')[0]}</p>
+      </div>
+      <hr className="my-3 w-full border-t border-btn-primary-inactive-color" />
       <StockTutorialResultItem />
     </div>
   );
