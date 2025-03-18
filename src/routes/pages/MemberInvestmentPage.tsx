2b9e2fa77c6d2c9d8a64e7f5db985263ccac13ef
frndhoon <frndhoon@gmail.com>
2025-03-18 17:52:16 +0900

    feat: 투자 내용 일부 구현(temp)

--- src/routes/pages/MemberInvestmentPage.tsx
+++ src/routes/pages/MemberInvestmentPage.tsx
@@ -1,3 +1,44 @@
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+
 export const MemberInvestmentPage = () => {
-  return <div>MemberInvestmentPage</div>;
+  return (
+    <div className="flex w-full flex-col gap-4">
+      <div className="flex flex-row gap-3">
+        <div className="flex flex-col items-start">
+          <p>총 자산</p>
+          <p>52,1223,425원</p>
+        </div>
+        <div className="flex flex-row items-start rounded-lg bg-modal-background-color p-3">
+          <div className="flex flex-col items-start">
+            <p>내 평가금</p>
+            <p>32,123,425원</p>
+          </div>
+          <hr className="h-full w-1 border-r border-btn-primary-inactive-color" />
+          <div className="flex  flex-col items-start">
+            <p>내 현금</p>
+            <p>32,123,425원</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row gap-3">
+          <Badge variant="increase">총 수익률: +32.12%</Badge>
+          <Badge variant="main">총 수익: 50,000,000원</Badge>
+          <Badge variant="decrease">일간 수익률: -32.12%</Badge>
+          <Badge variant="main">일간 수익: -5,000원</Badge>
+        </div>
+        <div className="flex flex-row gap-3">
+          <Button variant="red">자산 초기화 하기</Button>
+        </div>
+      </div>
+      <hr className="w-full border-btn-primary-inactive-color" />
+      <div className="flex flex-row gap-3">
+        <div className="flex flex-col items-start">
+          <p>총 자산</p>
+          <p>52,1223,425원</p>
+        </div>
+      </div>
+    </div>
+  );
 };
