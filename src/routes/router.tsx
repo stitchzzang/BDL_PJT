4f2fb4986a17c624750ea620e0987d1a7abbac0c
frndhoon <frndhoon@gmail.com>
2025-03-18 13:03:59 +0900

    feat: 회원가입 페이지 생성 및 라우팅 설정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -6,6 +6,7 @@ import { HomePage } from '@/routes/pages/HomePage';
 import { LoginPage } from '@/routes/pages/LoginPage';
 import { NotFoundPage } from '@/routes/pages/NotFoundPage';
 import { SearchPage } from '@/routes/pages/SearchPage';
+import { SignUpPage } from '@/routes/pages/SignUpPage';
 import { SimulatedEducationPage } from '@/routes/pages/SimulatedEducationPage';
 import { SimulatedInvestmentPage } from '@/routes/pages/SimulatedInvestmentPage';
 
@@ -25,6 +26,10 @@ export const router = createBrowserRouter([
       {
         path: '/algorithm-lab',
         element: <AlgorithmLabPage />,
+      },
+      {
+        path: '/signup',
+        element: <SignUpPage />,
       },
       {
         path: '/simulated-education',
