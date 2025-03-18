495ca6e988b39bfa33668f1bf4d698218af180cd
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-18 10:30:23 +0900

    feat:각 컴포넌트 매핑 완료

--- src/components/home-page/real-time-chart/real-time-chart.tsx
+++ src/components/home-page/real-time-chart/real-time-chart.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 
+import { RealTimeChartFall } from '@/components/home-page/real-time-chart/real-time-chart-fall';
+import { RealTimeChartRise } from '@/components/home-page/real-time-chart/real-time-chart-rise';
+import { RealTimeChartTransaction } from '@/components/home-page/real-time-chart/real-time-chart-transaction';
+
 // 차트 타입 정의
 enum chartType {
   TRADING_VOLUME = '거래대금',
@@ -11,11 +15,12 @@ export const RealTimeChart = () => {
   const [isActive, setIsActive] = useState<string>('거래대금');
 
   // Record를 사용하여 컴포넌트 매핑
-  const components : Record<chartType, React.ReactNode> = {
-    [chartType.TRADING_VOLUME]:
-    [chartType.RAPID_RISE]:
-    [chartType.RAPID_FALL]:
-  }
+  const components: Record<chartType, React.ReactNode> = {
+    [chartType.TRADING_VOLUME]: <RealTimeChartTransaction />,
+    [chartType.RAPID_RISE]: <RealTimeChartRise />,
+    [chartType.RAPID_FALL]: <RealTimeChartFall />,
+  };
+
   return (
     <div>
       <div></div>
