0b0e4ec86aea1fdede45f5688d1dad1591754550
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-18 13:20:49 +0900

    feat:기본 구조 완료

--- src/components/home-page/real-time-chart/real-time-chart-fall.tsx
+++ src/components/home-page/real-time-chart/real-time-chart-fall.tsx
@@ -1,3 +1,27 @@
+export interface StockFall {
+  stockName: string;
+  currentPrice: number;
+  changeRate: number;
+  imageUrl: string | null;
+}
+
+export type StockFalls = StockFall;
+
+const StockFalls: StockFalls[] = [
+  {
+    stockName: '삼성전자',
+    currentPrice: 140203010,
+    changeRate: 3.15,
+    imageUrl: null,
+  },
+  {
+    stockName: '삼성전자',
+    currentPrice: 140203010,
+    changeRate: 3.15,
+    imageUrl: null,
+  },
+];
+
 export const RealTimeChartFall = () => {
   return (
     <div>
