bc3de7903ffd579dbc818eb7345dce84652a38d8
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-19 12:58:34 +0900

    desgin: 메인 컴포넌트 구조 디자인

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -4,18 +4,22 @@ import { RealTimeChart } from '@/components/home-page/real-time-chart/real-time-
 
 export const HomePage = () => {
   return (
-    <div>
+    <div className="px-4">
       <div>{/* 코스피,코스단.. 차트 */}</div>
       <div>
-        <div>
-          <div>
+        <div className="w-full">
+          <div className="mb-[25px] flex items-center gap-3">
             <h3 className="text-[28px] font-bold">실시간 차트</h3>
             <p className="text-[20px] font-light text-text-inactive-2-color">어제 08:25 기준</p>
           </div>
-          <RealTimeChart />
-        </div>
-        <div>
-          <NewsChart />
+          <div className="grid w-full grid-cols-1 gap-3 border lg:grid-cols-7">
+            <div className="col-span-1 lg:col-span-5">
+              <RealTimeChart />
+            </div>
+            <div className="col-span-1 lg:col-span-2">
+              <NewsChart />
+            </div>
+          </div>
         </div>
       </div>
       <div>
