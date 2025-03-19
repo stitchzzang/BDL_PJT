a41d915bb21242c2645cb9aa6ae70d4864fa0ad6
frndhoon <frndhoon@gmail.com>
2025-03-19 15:56:11 +0900

    feat: 검색페이지 임시

--- src/routes/pages/SearchPage.tsx
+++ src/routes/pages/SearchPage.tsx
@@ -1,3 +1,16 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <button className="text-text-inactive-color hover:text-text-main-color active:text-text-main-color">
+      <MagnifyingGlassIcon className="h-5 w-5 text-[#718096]" />
+      <input
+        className="bg-transparent text-[#718096] focus:outline-none"
+        type="text"
+        name="search"
+        id="search"
+        placeholder="기업을 검색하세요."
+      />
+    </button>
+  );
 };
