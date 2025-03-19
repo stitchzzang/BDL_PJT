63793a1c26b56d70bee21b9bc3d94ca6e9cce309
frndhoon <frndhoon@gmail.com>
2025-03-19 12:54:51 +0900

    feat: hompage 문구 제거

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,11 +1,10 @@
-import { RankCards } from '@/components/home-page/rank-cards/rank-cards';
 import { NewsChart } from '@/components/home-page/news/news-chart';
+import { RankCards } from '@/components/home-page/rank-cards/rank-cards';
 import { RealTimeChart } from '@/components/home-page/real-time-chart/real-time-chart';
 
 export const HomePage = () => {
   return (
     <div>
-      <div>HomePage</div>
       <div>
         <RealTimeChart />
       </div>
