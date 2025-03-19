705255f403a0e53bece4a94dbaeba906b798da20
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-19 15:18:21 +0900

    design:레이아웃 퍼블리싱

--- src/routes/pages/SimulatedInvestmentPage.tsx
+++ src/routes/pages/SimulatedInvestmentPage.tsx
@@ -1,10 +1,10 @@
+import { OrderStatus } from '@/components/mock-investment/order-status/order-status';
 import { SellingPrice } from '@/components/mock-investment/selling-price/selling-price';
 import { StockCostHistory } from '@/components/mock-investment/stock-cost-history/stock-cost-history';
 import { StockInfo } from '@/components/mock-investment/stock-info/stock-info';
 import { StockInfoDetail } from '@/components/mock-investment/stock-info-detail/stock-info-detail';
 import ChartComponent from '@/components/ui/chart';
 import { dummyChartData } from '@/lib/dummy-data';
-import { OrderStatus } from '@/components/mock-investment/order-status/order-status';
 
 export const SimulatedInvestmentPage = () => {
   return (
@@ -25,22 +25,22 @@ export const SimulatedInvestmentPage = () => {
           </div>
         </div>
       </div>
-      <div className="flex-1">
-        <div>
+      <div className="mb-[20px] grid grid-cols-10 gap-5">
+        <div className="col-span-8">
           <ChartComponent data={dummyChartData} height={600} />
         </div>
-        <div>
+        <div className="col-span-2">
           <OrderStatus />
         </div>
       </div>
-      <div>
-        <div>
+      <div className="grid grid-cols-10 gap-5">
+        <div className="col-span-4">
           <StockCostHistory />
         </div>
-        <div>
+        <div className="col-span-4">
           <StockInfoDetail />
         </div>
-        <div>
+        <div className="col-span-2">
           <SellingPrice />
         </div>
       </div>
