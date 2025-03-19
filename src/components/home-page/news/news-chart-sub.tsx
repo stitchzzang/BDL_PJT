2c9e3f75f0d6f075b05690c4d599ec48039c0c70
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-19 10:50:11 +0900

    feat:interface 구조화

--- src/components/home-page/news/news-chart-sub.tsx
+++ src/components/home-page/news/news-chart-sub.tsx
@@ -1,7 +1,16 @@
-export const NewsChartSub = () => {
+import NoneImg from '@/assets/none-img/none_stock_img.png';
+import { NewsList } from '@/components/home-page/news/news-chart';
+
+export interface NewsChartSubProps {
+  newsSubInfo: NewsList;
+}
+
+export const NewsChartSub = ({ newsSubInfo }: NewsChartSubProps) => {
   return (
     <div>
-      <div></div>
+      <div className="max-h-[100px] max-w-[100px]">
+        <img src={NoneImg} alt="" />
+      </div>
     </div>
   );
 };
