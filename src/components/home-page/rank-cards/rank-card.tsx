b776787768016d50dc2212a1084d1ebd80639895
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-19 10:07:29 +0900

    desgin:기본 스타일링

--- src/components/home-page/rank-cards/rank-card.tsx
+++ src/components/home-page/rank-cards/rank-card.tsx
@@ -1,3 +1,4 @@
+import noneStockImg from '@/assets/none-img/none_stock_img.png';
 import { RankInformation } from '@/components/home-page/rank-cards/rank-cards';
 
 interface RankCardProps {
@@ -8,10 +9,18 @@ export const RankCard = ({ rankInfo }: RankCardProps) => {
   const { name, rate, imgUrl } = rankInfo;
   return (
     <div className="flex items-center justify-center rounded-2xl bg-modal-background-color p-[30px]">
-      <div className="flex flex-col items-center justify-center">
-        <p>{name}</p>
-        <p>{rate}</p>
-        <p>{imgUrl}</p>
+      <div className="flex flex-col items-center justify-center gap-[15px]">
+        {imgUrl === null ? (
+          <div className="max-h-[168px] max-w-[168px] overflow-hidden rounded-2xl">
+            <img src={noneStockImg} alt="noneimg" />
+          </div>
+        ) : (
+          <img src={imgUrl} alt="profilimg" />
+        )}
+        <p className="text-[22px] font-medium">{name}</p>
+        <div className="rounded-2xl border border-btn-red-color px-[28px] py-[12px]">
+          <p className="text-[28px] font-bold text-btn-red-color">{rate} %</p>
+        </div>
       </div>
     </div>
   );
