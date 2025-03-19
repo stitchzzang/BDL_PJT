32741fa6d7522c1da60fb0d44f4057505692465e
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-19 10:29:16 +0900

    test:인터페이스 구성 및 테스트 데이터 추가

--- src/components/home-page/news/news-chart.tsx
+++ src/components/home-page/news/news-chart.tsx
@@ -1,5 +1,24 @@
 import { NewsChartMain } from '@/components/home-page/news/news-chart-main';
 
+export interface NewsList {
+  imgUrl: string | null;
+  title: string;
+  subject: string;
+}
+
+const NewsLists: NewsList[] = [
+  {
+    imgUrl: null,
+    title: 'AI 신흥강자 브로드컴 호실적, 월가 강세 전망 강화…주가 8%↑',
+    subject: '뉴스내용 뉴스내용 뉴스내용 뉴스내용 뉴스내용 뉴스내용 뉴스내용 ',
+  },
+  {
+    imgUrl: null,
+    title: 'AI 신흥강자 브로드컴 호실적, 월가 강세 전망 강화…주가 8%↑',
+    subject: '뉴스내용 뉴스내용 뉴스내용 뉴스내용 뉴스내용 뉴스내용 뉴스내용 ',
+  },
+];
+
 export const NewsChart = () => {
   return (
     <div>
