b20eea87a7af431212b8531f3d19546cbbb1e324
신해인 <godls0215@naver.com>
2025-03-19 15:20:37 +0900

    feat: 모의교육 더미데이터 연동

--- src/routes/pages/SimulatedEducationPage.tsx
+++ src/routes/pages/SimulatedEducationPage.tsx
@@ -1,11 +1,11 @@
 import ChartComponent from '@/components/ui/chart';
-import { minuteData } from '@/lib/dummy-data';
+import { dummyChartData } from '@/lib/dummy-data';
 
 export const SimulatedEducationPage = () => {
   return (
     <div className="flex h-full w-full flex-col">
       <div className="flex-1">
-        <ChartComponent data={minuteData} height={600} />
+        <ChartComponent data={dummyChartData} height={600} />
       </div>
     </div>
   );
