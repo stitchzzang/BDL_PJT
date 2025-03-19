2416cb062a2d501d1a5837e7203d3fbebed77741
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-19 13:53:24 +0900

    feat:desgin:그리드 시스템 수정

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -7,12 +7,12 @@ export const HomePage = () => {
     <div className="px-4">
       <div>{/* 코스피,코스단.. 차트 */}</div>
       <div>
-        <div className="w-full">
+        <div className="mt-[100px] w-full">
           <div className="mb-[25px] flex items-center gap-3">
             <h3 className="text-[28px] font-bold">실시간 차트</h3>
             <p className="text-[20px] font-light text-text-inactive-2-color">어제 08:25 기준</p>
           </div>
-          <div className="grid min-h-0 w-full grid-cols-1 gap-3 border md:grid-cols-7 lg:min-h-[520px]">
+          <div className="grid min-h-0 w-full grid-cols-1 gap-3 lg:min-h-[520px] lg:grid-cols-7">
             <div className="col-span-1 lg:col-span-5">
               <RealTimeChart />
             </div>
@@ -22,8 +22,18 @@ export const HomePage = () => {
           </div>
         </div>
       </div>
-      <div>
-        <RankCards />
+      <div className="mt-[100px] w-full">
+        <div className="mb-[25px] flex flex-col gap-1">
+          <h3 className="text-[28px] font-bold">
+            수익률 랭킹 <span className="text-btn-red-color">TOP5</span>
+          </h3>
+          <p className="text-[20px] font-light text-text-inactive-2-color">
+            BDL에서 가장 높은 수입률을 자랑하는 유저를 소개합니다.
+          </p>
+        </div>
+        <div>
+          <RankCards />
+        </div>
       </div>
     </div>
   );
