7810002b0ebf26a176e1baba722e72dfa510d80e
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-19 10:40:02 +0900

    desgin:디자인 스타일링 작업

--- src/components/home-page/news/news-chart-main.tsx
+++ src/components/home-page/news/news-chart-main.tsx
@@ -1,7 +1,28 @@
-export const NewsChartMain = () => {
+import noneNewsImg from '@/assets/none-img/none_news_img.png';
+import { NewsList } from '@/components/home-page/news/news-chart';
+
+export interface NewsChartMainProps {
+  newsMainInfo: NewsList;
+}
+
+export const NewsChartMain = ({ newsMainInfo }: NewsChartMainProps) => {
   return (
-    <div>
-      <div></div>
+    <div className="w-full">
+      <div className="inline-block overflow-hidden rounded-2xl">
+        {newsMainInfo.imgUrl === null ? (
+          <img src={noneNewsImg} alt="noneImg" />
+        ) : (
+          <img src={newsMainInfo.imgUrl} alt="newsMainImg" />
+        )}
+      </div>
+      <div>
+        <h3 className="whitespace-normal break-words text-[24px] font-bold">
+          {newsMainInfo.title}
+        </h3>
+        <p className="whitespace-normal break-words text-[16px] font-light">
+          {newsMainInfo.subject}
+        </p>
+      </div>
     </div>
   );
 };
