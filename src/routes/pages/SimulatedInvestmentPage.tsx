3ea5343044a29ad54976cfb550d67a55ae2835e6
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-19 15:13:17 +0900

    feat:모의 주식 컴포넌트 배치

--- src/routes/pages/SimulatedInvestmentPage.tsx
+++ src/routes/pages/SimulatedInvestmentPage.tsx
@@ -1,11 +1,48 @@
+import { SellingPrice } from '@/components/mock-investment/selling-price/selling-price';
+import { StockCostHistory } from '@/components/mock-investment/stock-cost-history/stock-cost-history';
+import { StockInfo } from '@/components/mock-investment/stock-info/stock-info';
+import { StockInfoDetail } from '@/components/mock-investment/stock-info-detail/stock-info-detail';
 import ChartComponent from '@/components/ui/chart';
 import { dummyChartData } from '@/lib/dummy-data';
+import { OrderStatus } from '@/components/mock-investment/order-status/order-status';
 
 export const SimulatedInvestmentPage = () => {
   return (
     <div className="flex h-full w-full flex-col">
+      <div>
+        <div>
+          <StockInfo />
+        </div>
+        <div>
+          <div>
+            <p>차트</p>
+          </div>
+          <div>
+            <p>오늘 날짜</p>
+            <div>
+              <p>2023:02:12</p>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="flex-1">
-        <ChartComponent data={dummyChartData} height={600} />
+        <div>
+          <ChartComponent data={dummyChartData} height={600} />
+        </div>
+        <div>
+          <OrderStatus />
+        </div>
+      </div>
+      <div>
+        <div>
+          <StockCostHistory />
+        </div>
+        <div>
+          <StockInfoDetail />
+        </div>
+        <div>
+          <SellingPrice />
+        </div>
       </div>
     </div>
   );
