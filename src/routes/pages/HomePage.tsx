4921f33c91c3f1acf6946db08f5eb8b62d421cea
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-19 12:38:37 +0900

    feat:메인페이지 기초 구조

--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,19 +1,25 @@
-import { RankCards } from '@/components/home-page/rank-cards/rank-cards';
 import { NewsChart } from '@/components/home-page/news/news-chart';
+import { RankCards } from '@/components/home-page/rank-cards/rank-cards';
 import { RealTimeChart } from '@/components/home-page/real-time-chart/real-time-chart';
 
 export const HomePage = () => {
   return (
     <div>
-      <div>HomePage</div>
+      <div>{/* 코스피,코스단.. 차트 */}</div>
       <div>
-        <RealTimeChart />
+        <div>
+          <div>
+            <h3 className="text-[28px] font-bold">실시간 차트</h3>
+            <p className="text-[20px] font-light text-text-inactive-2-color">어제 08:25 기준</p>
+          </div>
+          <RealTimeChart />
+        </div>
+        <div>
+          <NewsChart />
+        </div>
       </div>
       <div>
         <RankCards />
-      </div>
-      <div>
-        <NewsChart />
       </div>
     </div>
   );
