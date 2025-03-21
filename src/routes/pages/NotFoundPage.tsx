bd83ec1b98e3913e986b355ca7d5ac44ab9edd78
frndhoon <frndhoon@gmail.com>
2025-03-21 14:51:22 +0900

    feat: 404 page 퍼블리싱

--- src/routes/pages/NotFoundPage.tsx
+++ src/routes/pages/NotFoundPage.tsx
@@ -1,3 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '@/components/ui/button';
+import { Lanyard } from '@/components/ui/lanyard';
+
 export const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-2 overflow-hidden">
+      <div className="w-[500px]">
+        <Lanyard />
+      </div>
+      <div className="flex flex-col items-center justify-center gap-6">
+        <h1 className="text-5xl font-bold text-primary-color">404</h1>
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-3xl font-medium">페이지를 찾을 수 없습니다.</p>
+          <p className="text-lg text-text-inactive-3-color">요청하신 페이지가 존재하지 않습니다.</p>
+        </div>
+        <Button variant="blue" onClick={() => navigate('/')} className="mt-4 px-8 py-6 text-lg">
+          홈으로 돌아가기
+        </Button>
+      </div>
+    </div>
+  );
 };
