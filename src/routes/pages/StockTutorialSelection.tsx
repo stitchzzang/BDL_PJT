0ecd72556a6eb962b6a2336db00935d52f326e0d
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-24 09:45:42 +0900

    feat:이미지 추가

--- src/routes/pages/StockTutorialSelection.tsx
+++ src/routes/pages/StockTutorialSelection.tsx
@@ -1,7 +1,22 @@
+import NoneLogo from '/none-img/none-logo.png';
+
 export const StockTutorialSelection = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <div>
+          <h1>주식 튜토리얼</h1>
+          <p>주식에 익숙하지 않은 당식을 위하여여</p>
+          <span>주식에 익숙하지 않은 당식을 위하여여</span>
+          <p>
+            먼저 경험하고 싶은 <span>카테고리</span>를 골라볼까요?
+          </p>
+        </div>
+        <div>{/* 카테고리 선택 */}</div>
+      </div>
+      <div>
+        <img src={NoneLogo} alt="" />
+      </div>
     </div>
   );
 };
