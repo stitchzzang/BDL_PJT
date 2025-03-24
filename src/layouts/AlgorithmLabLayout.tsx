ffddb6786f7514e940f23d67b6034ab1cfefa4c0
frndhoon <frndhoon@gmail.com>
2025-03-24 10:35:33 +0900

    design: algorithm lab max width 데스크탑/모바일 동일하게 조정

--- src/layouts/AlgorithmLabLayout.tsx
+++ src/layouts/AlgorithmLabLayout.tsx
@@ -2,8 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 export const AlgorithmLabLayout = () => {
   return (
-    <div className="container mx-auto py-8">
-      <Outlet />
+    <div className="flex w-full justify-center">
+      <div className="w-full max-w-[500px] px-4 py-8">
+        <Outlet />
+      </div>
     </div>
   );
 };
