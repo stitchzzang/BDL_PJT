92a3a564425969951e15028567ba7356c667ca29
frndhoon <frndhoon@gmail.com>
2025-03-24 16:51:12 +0900

    chore: webapp width px 추가(860px)

--- tailwind.config.js
+++ tailwind.config.js
@@ -68,6 +68,9 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      screens: {
+        webapp: '860px',
+      },
     },
   },
   plugins: [tailwindAnimate],
