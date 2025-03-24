21d8c55cb4a5423c4d19315e21ff16310e88ec85
frndhoon <frndhoon@gmail.com>
2025-03-24 17:26:28 +0900

    fix: build error 수정
    - ts2741 error 수정

--- src/routes/pages/SearchPage.tsx
+++ src/routes/pages/SearchPage.tsx
@@ -23,7 +23,7 @@ export const SearchPage = () => {
             <MagnifyingGlassIcon className="h-5 w-5 text-[#718096]" />
           </button>
         </div>
-        <CategoryList />
+        <CategoryList setCategory={() => {}} />
         <p className="my-3 text-lg text-[#718096]">카테고리 선택으로도 검색이 가능합니다.</p>
         <CompanySelectButton />
       </div>
