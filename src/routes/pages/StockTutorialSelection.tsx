df15d507f343e458250ae05a82f17e1b62f35dd4
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-24 09:51:13 +0900

    design:기본 스타일링

--- src/routes/pages/StockTutorialSelection.tsx
+++ src/routes/pages/StockTutorialSelection.tsx
@@ -1,21 +1,26 @@
 import NoneLogo from '/none-img/none-logo.png';
+import { CategoryList } from '@/components/common/category-list';
 
 export const StockTutorialSelection = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <h1>주식 튜토리얼</h1>
-          <p>주식에 익숙하지 않은 당식을 위하여여</p>
-          <span>주식에 익숙하지 않은 당식을 위하여여</span>
-          <p>
-            먼저 경험하고 싶은 <span>카테고리</span>를 골라볼까요?
+    <div className="flex flex-col items-center justify-center gap-3">
+      <div className="flex flex-col items-center justify-center gap-3">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="mb-[10px] text-[28px] font-bold">주식 튜토리얼</h1>
+          <p className="text-[16px]">주식에 익숙하지 않은 당식을 위하여여</p>
+          <span className="text-[16px] font-bold">적응을 도와드릴게요.</span>
+          <p className="text-[16px]">
+            먼저 경험하고 싶은{' '}
+            <span className="text-[16px] font-bold text-btn-blue-color">카테고리</span>를
+            골라볼까요?
           </p>
         </div>
-        <div>{/* 카테고리 선택 */}</div>
+        <div>
+          <CategoryList />
+        </div>
       </div>
-      <div>
-        <img src={NoneLogo} alt="" />
+      <div className="mt-[50px]">
+        <img src={NoneLogo} alt="none-logo" />
       </div>
     </div>
   );
