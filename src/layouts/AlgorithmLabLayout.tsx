527410d7125d468dd35f2cdfcc6f75b2e796e17c
frndhoon <frndhoon@gmail.com>
2025-03-24 10:33:05 +0900

    feat: 알고리즘 랩 layout 추가

---
+++ src/layouts/AlgorithmLabLayout.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from 'react-router-dom';
+
+export const AlgorithmLabLayout = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <Outlet />
+    </div>
+  );
+};
