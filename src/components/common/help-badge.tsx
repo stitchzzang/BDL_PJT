2cd8879e5ca5eb03d43783be7ccd8a9b8ba7f38e
frndhoon <frndhoon@gmail.com>
2025-03-24 13:41:45 +0900

    feat: HelpBadge 컴포넌트에서 설명을 여러 부분으로 나누어 표시하도록 수정

--- src/components/common/help-badge.tsx
+++ src/components/common/help-badge.tsx
@@ -12,12 +12,28 @@ export const HelpBadge = ({
   description: string;
   bgColor?: string;
 }) => {
+  const descriptionParts = description.split('\n\n');
+
   return (
     <div
       className={`flex w-full flex-col gap-2 rounded-2xl border border-btn-primary-inactive-color p-5 ${bgColor}`}
     >
       <p className="text-lg font-bold">{title}</p>
-      <p className="whitespace-pre-line text-sm">{description}</p>
+      <div className="flex flex-col gap-2">
+        {descriptionParts.map((part, index) => (
+          <>
+            <p
+              key={index}
+              className={`whitespace-pre-line text-sm ${index > 0 ? 'text-btn-primary-active-color' : ''}`}
+            >
+              {part}
+            </p>
+            {index < descriptionParts.length - 1 && (
+              <hr className="my-1 border-btn-primary-active-color" />
+            )}
+          </>
+        ))}
+      </div>
     </div>
   );
 };
