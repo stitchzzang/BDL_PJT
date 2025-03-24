5bcc435a4fdd2d8f76206ff4d3f97ed1f729a541
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-24 10:55:49 +0900

    feat:기본 구조 정리

--- src/components/stock-tutorial/stock-tutorial-money-info.tsx
+++ src/components/stock-tutorial/stock-tutorial-money-info.tsx
@@ -1,7 +1,18 @@
 export const StockTutorialMoneyInfo = () => {
   return (
-    <div>
-      <div></div>
+    <div className="flex gap-3">
+      <div className="flex gap-2 rounded-xl bg-modal-background-color px-[15px] py-[10px]">
+        <p className="text-border-color">시드머니:</p>
+        <span className="font-bold">5000000000원</span>
+      </div>
+      <div className="flex gap-2 rounded-xl bg-modal-background-color px-[15px] py-[10px]">
+        <p className="text-border-color">주문가능능:</p>
+        <span className="font-bold">5000000000원</span>
+      </div>
+      <div className="flex gap-2 rounded-xl bg-modal-background-color px-[15px] py-[10px]">
+        <p className="text-border-color">현재자산:</p>
+        <span className="font-bold">5000000000원</span>
+      </div>
     </div>
   );
 };
