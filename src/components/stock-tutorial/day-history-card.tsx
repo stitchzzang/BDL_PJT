2f894d1480467e832b4f41058cb2c39648baa099
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-24 12:07:59 +0900

    design:카드 디자인

--- src/components/stock-tutorial/day-history-card.tsx
+++ src/components/stock-tutorial/day-history-card.tsx
@@ -1,7 +1,10 @@
 export const DayHistoryCard = () => {
   return (
     <div>
-      <div></div>
+      <div className="flex justify-between gap-2 rounded-lg border border-btn-red-color px-[15px] py-[5px]">
+        <h1 className="text-[16px] text-btn-red-color">뉴스제목</h1>
+        <p className="text-[16px] text-border-color">2/12 - 13:03</p>
+      </div>
     </div>
   );
 };
