0ba6b0cbdcd4f09d527c83bc4ee10be80bfcc247
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-24 13:02:25 +0900

    design:카드 디자인

--- src/components/stock-tutorial/stock-tutorial-conclusion-card.tsx
+++ src/components/stock-tutorial/stock-tutorial-conclusion-card.tsx
@@ -1,7 +1,10 @@
-export const StockTutorialConclustionCard = () => {
+export const StockTutorialConclusionCard = () => {
   return (
-    <div>
-      <div></div>
+    <div className="rounded-xl bg-btn-blue-color bg-opacity-20 px-[20px] py-[10px]">
+      <div className="flex justify-between">
+        <h3>12,321원 | 3주</h3>
+        <p className="text-btn-blue-color">판매</p>
+      </div>
     </div>
   );
 };
--- src/components/stock-tutorial/stock-tutorial-conclusion.tsx
+++ src/components/stock-tutorial/stock-tutorial-conclusion.tsx
@@ -1,10 +1,14 @@
+import { StockTutorialConclusionCard } from '@/components/stock-tutorial/stock-tutorial-conclusion-card';
+
 export const StockTutorialConclusion = () => {
   return (
     <div>
       <div className="flex items-center gap-3">
         <h1 className="text-[18px] font-bold">체결내역</h1>
       </div>
-      <div></div>
+      <div>
+        <StockTutorialConclusionCard />
+      </div>
     </div>
   );
 };
