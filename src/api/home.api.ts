8ad1340238c2ad9ec56516af28cb76f8deba9d10
frndhoon <frndhoon@gmail.com>
2025-03-25 13:04:45 +0900

    feat: home page 쪽 api 생성

---
+++ src/api/home.api.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { _ky } from '@/api/instance';
+import { ApiResponse } from '@/api/types/common';
+import { LatestNews } from '@/api/types/home';
+
+export const homeApi = {
+  getLatestNews: () => _ky.get('news/latest').json<ApiResponse<LatestNews[]>>(),
+};
+
+export const useLatestNews = () => {
+  return useQuery({
+    queryKey: ['news', 'latest'],
+    queryFn: () => homeApi.getLatestNews().then((res) => res.result),
+  });
+};
