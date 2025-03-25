562eaa8325910d0b25ffbfebd1c7d1d1f410b4b4
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-25 14:25:05 +0900

    design:디자인 변경

--- src/components/mock-investment/stock-chart/stock-chart.tsx
+++ src/components/mock-investment/stock-chart/stock-chart.tsx
@@ -168,20 +168,21 @@ export const StockChart = () => {
   };
 
   return (
-    <div className="">
+    <div className="flex items-center gap-4">
       <div>
         {message === null ? (
           <p>none</p>
         ) : (
           <div
-            className={`rounded-2xl bg-opacity-60 p-3 transition-all duration-200 ${message.ccldDvsn === '1' ? 'bg-btn-red-color' : 'bg-btn-blue-color'}`}
+            className={`flex gap-3 rounded-2xl border-2 bg-opacity-40 p-3 px-6 transition-all duration-200 ${message.ccldDvsn === '1' ? 'border-btn-red-color text-btn-red-color' : 'border-btn-blue-color text-btn-blue-color'}`}
           >
             <h1>{message.stckPrpr}</h1>
-            <h1>{message.ccldDvsn}</h1>
+            <h1 className="text-white opacity-60">{message.cntgVol}</h1>
           </div>
         )}
         {tradeData.length === 0 ? <p>데이터를 기다리는 중...</p> : <ul></ul>}
       </div>
+      <p className="opacity-60">현재 거래</p>
     </div>
   );
 };
