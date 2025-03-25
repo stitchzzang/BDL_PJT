e57943074c432c0affec6cd4717fbf769d25f84d
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-25 15:42:58 +0900

    test:문자열 테스트

--- src/api/stock.api.ts
+++ src/api/stock.api.ts
@@ -6,13 +6,15 @@ import { StockMinuteData } from '@/api/types/stock';
 
 export const StockApi = {
   // 분봉 데이터 가져오기 (limit 값은 직접 입력)
-  getStockMinuteData: (limit: number) =>
-    _ky.get(`stocks/000660/minute/initial?limit=${limit}`).json<ApiResponse<StockMinuteData[]>>(),
+  getStockMinuteData: (stockId: string, limit: number) =>
+    _ky
+      .get(`stocks/${stockId}/minute/initial?limit=${limit}`)
+      .json<ApiResponse<StockMinuteData[]>>(),
 };
 
 export const useStockMinuteData = () => {
   return useQuery({
     queryKey: ['stockMinData'],
-    queryFn: () => StockApi.getStockMinuteData(50).then((res) => res.result),
+    queryFn: () => StockApi.getStockMinuteData('000660', 50).then((res) => res.result),
   });
 };
