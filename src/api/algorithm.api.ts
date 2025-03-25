2f9ca6da9347db58744d12bffa480680cbfa7cbd
frndhoon <frndhoon@gmail.com>
2025-03-25 14:16:12 +0900

    feat: 내 algorithm 정보 가져오는 api 추가

--- src/api/algorithm.api.ts
+++ src/api/algorithm.api.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 import { _ky } from '@/api/instance';
 import { Algorithm } from '@/api/types/algorithm';
@@ -7,11 +7,20 @@ import { ApiResponse } from '@/api/types/common';
 export const algorithmAPI = {
   createAlgorithm: (memberId: string, algorithm: Algorithm) =>
     _ky.post(`algorithm/${memberId}`, { json: algorithm }).json<ApiResponse<Algorithm>>(),
+  getAlgorithm: (memberId: string) =>
+    _ky.get(`algorithm/${memberId}`).json<ApiResponse<Algorithm[]>>(),
 };
 
 export const useCreateAlgorithm = () => {
   return useMutation({
     mutationFn: ({ memberId, algorithm }: { memberId: string; algorithm: Algorithm }) =>
       algorithmAPI.createAlgorithm(memberId, algorithm).then((res) => res.result),
+  });
+};
+
+export const useGetAlgorithm = (memberId: string) => {
+  return useQuery<Algorithm[]>({
+    queryKey: ['algorithms'],
+    queryFn: () => algorithmAPI.getAlgorithm(memberId).then((res) => res.result),
   });
 };
