57bcb62abe22178a17c1d50a8c4b96dfa2289680
WJG <dnwnsrb11@naver.com>
2025-03-25 20:10:44 +0900

    feat:tick props 전달

--- src/components/mock-investment/stock-chart/stock-chart.tsx
+++ src/components/mock-investment/stock-chart/stock-chart.tsx
@@ -5,21 +5,21 @@ interface TickInfoProps {
   TickData: TickData;
 }
 
-export const TickInfo = () => {
+export const TickInfo = ({ TickData }: TickInfoProps) => {
   return (
     <div className="flex items-center gap-4">
       <div>
-        {message === null ? (
+        {TickData === null ? (
           <p>none</p>
         ) : (
           <div
-            className={`flex gap-3 rounded-2xl border-2 bg-opacity-40 p-3 px-6 transition-all duration-200 ${message.ccldDvsn === '1' ? 'border-btn-red-color text-btn-red-color' : 'border-btn-blue-color text-btn-blue-color'}`}
+            className={`flex gap-3 rounded-2xl border-2 bg-opacity-40 p-3 px-6 transition-all duration-200 ${TickData.ccldDvsn === '1' ? 'border-btn-red-color text-btn-red-color' : 'border-btn-blue-color text-btn-blue-color'}`}
           >
-            <h1>{message.stckPrpr}</h1>
-            <h1 className="text-white opacity-60">{message.cntgVol}</h1>
+            <h1>{TickData.stckPrpr}</h1>
+            <h1 className="text-white opacity-60">{TickData.cntgVol}</h1>
           </div>
         )}
-        {tradeData.length === 0 ? <p>데이터를 기다리는 중...</p> : <ul></ul>}
+        {TickData === null ? <p>데이터를 기다리는 중...</p> : <ul></ul>}
       </div>
       <p className="opacity-60">현재 거래</p>
     </div>
