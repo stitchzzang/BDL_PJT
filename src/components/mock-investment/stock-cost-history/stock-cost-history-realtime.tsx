f8f3b41a785d884d23cf213b9ec5f46116cd12c8
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-26 10:07:23 +0900

    design: 스크롤바 추가

--- src/components/mock-investment/stock-cost-history/stock-cost-history-realtime.tsx
+++ src/components/mock-investment/stock-cost-history/stock-cost-history-realtime.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
 import { TickData } from '@/api/types/stock';
+import { getFormatTime } from '@/utils/getTimeFormatted';
 import { addCommasToThousand, formatKoreanMoney } from '@/utils/numberFormatter';
 
 interface StockCostHistoryRealTimeProps {
@@ -15,7 +16,7 @@ export const StockCostHistoryRealTime = ({ tickData }: StockCostHistoryRealTimeP
   }, [tickData]);
   return (
     <div>
-      {!tickDataLists ? (
+      {!tickDataLists.length ? (
         <div>
           <h1>현재 데이터가 없습니다</h1>
         </div>
@@ -34,26 +35,31 @@ export const StockCostHistoryRealTime = ({ tickData }: StockCostHistoryRealTimeP
                 </div>
                 <div className="w-[20%] text-right text-[16px] text-border-color">시간</div>
               </div>
-
-              {/* 테이블 로우들 - 배열의 각 항목을 매핑 */}
-              {tickDataLists.map((item, index) => (
-                <div
-                  key={index}
-                  className="flex flex-row rounded-lg bg-[#102038] p-3 text-white hover:bg-modal-background-color"
-                >
-                  <div className="w-[20%] font-medium">{addCommasToThousand(item.stckPrpr)}원</div>
-                  <div className="w-[20%] text-right text-btn-blue-color">{item.cntgVol}</div>
-                  <div className="w-[20%] text-right text-btn-red-color">
-                    {addCommasToThousand(item.acmlVol)}
+              <div className="max-h-[500px] overflow-y-auto">
+                {/* 테이블 로우들 - 배열의 각 항목을 매핑 */}
+                {tickDataLists.map((item, index) => (
+                  <div
+                    key={index}
+                    className="my-2 flex flex-row rounded-lg bg-[#102038] p-3 text-white hover:bg-modal-background-color"
+                  >
+                    <div className="w-[20%] font-medium">
+                      {addCommasToThousand(item.stckPrpr)}원
+                    </div>
+                    <div
+                      className={`w-[20%] text-right text-btn-blue-color ${item.ccldDvsn === '1' ? 'text-btn-red-color' : 'text-btn-blue-color'}`}
+                    >
+                      {item.cntgVol}
+                    </div>
+                    <div className="w-[20%] text-right">{addCommasToThousand(item.acmlVol)}</div>
+                    <div className="w-[20%] text-right font-light text-border-color">
+                      {formatKoreanMoney(item.acmlTrPbm)}원
+                    </div>
+                    <div className="w-[20%] text-right font-light text-border-color">
+                      {getFormatTime(item.stckCntgHour)}
+                    </div>
                   </div>
-                  <div className="w-[20%] text-right font-light text-border-color">
-                    {formatKoreanMoney(item.acmlTrPbm)}원
-                  </div>
-                  <div className="w-[20%] text-right font-light text-border-color">
-                    {item.stckCntgHour}
-                  </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
