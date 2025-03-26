e29fcc354f6a84a7b3c3f27e83eeb16615552a8d
frndhoon <frndhoon@gmail.com>
2025-03-26 11:15:33 +0900

    feat: Auth 전역 상태에 프로필 사진 상태 추가

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -5,6 +5,7 @@ interface UserData {
   id: number | null;
   email: string | null;
   nickname: string | null;
+  profile: string | null;
 }
 
 interface AuthState {
@@ -20,7 +21,7 @@ type JWTPayload = UserData;
 
 export const useAuthStore = create<AuthState>((set) => ({
   // userData만 전역 상태 관리
-  userData: { id: null, email: null, nickname: null }, // const {id, email, nickname} = useAuthStore((state) => state.userData) 형식으로 사용
+  userData: { id: null, email: null, nickname: null, profile: null }, // const {id, email, nickname} = useAuthStore((state) => state.userData) 형식으로 사용
   isLogin: false,
   isInitialized: false,
 
@@ -34,7 +35,7 @@ export const useAuthStore = create<AuthState>((set) => ({
   // 토큰 제거 및 userData 초기화
   logoutAuth: () => {
     localStorage.removeItem('accessToken');
-    set({ userData: { id: null, email: null, nickname: null }, isLogin: false });
+    set({ userData: { id: null, email: null, nickname: null, profile: null }, isLogin: false });
   },
 
   // 토큰 및 userData 초기화 함수 (App.jsx에서 호출 됨에 따라 앱 마운트가 될 때 토큰으로 userData 업데이트)
