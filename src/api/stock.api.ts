17b5417dfc56066cba4dbdb9486e1e8fa0c72d24
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-26 14:55:27 +0900

    feat:지정가 api 정의

--- src/api/stock.api.ts
+++ src/api/stock.api.ts
@@ -1,8 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 import { _ky } from '@/api/instance';
 import { ApiResponse } from '@/api/types/common';
-import { StockMinuteData } from '@/api/types/stock';
+import { LimitOrderData, StockMinuteData } from '@/api/types/stock';
 
 export const StockApi = {
   // 분봉 데이터 가져오기 (limit 값은 직접 입력)
@@ -10,11 +10,39 @@ export const StockApi = {
     _ky
       .get(`stocks/${stockId}/minute/initial?limit=${limit}`)
       .json<ApiResponse<StockMinuteData[]>>(),
+
+  // 지정가 post
+  postStockLimitOrder: (
+    memberId: number, // 회원 ID
+    companyId: number, // 종목 ID
+    tradeType: number, // 0: 매수(구매), 1:매도(판매)
+    quantity: number, // 주 개수
+    price: number, // 지정가 - 가격
+  ) =>
+    _ky
+      .post(`simulated/limitorder`, {
+        json: {
+          memberId: memberId,
+          companyId: companyId,
+          tradeType: tradeType,
+          quantity: quantity,
+          price: price,
+        },
+      })
+      .json<ApiResponse<string>>(),
 };
 
 export const useStockMinuteData = (stockId: string, limit: number) => {
   return useQuery({
     queryKey: ['stockInitMinData'],
     queryFn: () => StockApi.getStockInitMinuteData(stockId, limit).then((res) => res.result),
+  });
+};
+
+// 지정가 판매,구매 api
+export const usePostStockLimitOrder = () => {
+  return useMutation({
+    mutationFn: ({ memberId, companyId, tradeType, quantity, price }: LimitOrderData) =>
+      StockApi.postStockLimitOrder(memberId, companyId, tradeType, quantity, price),
   });
 };
