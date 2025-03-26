2d342eaaebe0a3f649e445d8162a0eb7245872a7
frndhoon <frndhoon@gmail.com>
2025-03-26 14:33:37 +0900

    feat: 전체 카테고리 아이콘 및 ui 추가

--- src/components/common/category-list.tsx
+++ src/components/common/category-list.tsx
@@ -21,16 +21,33 @@ export const CategoryList = ({ setCategoryId: setCategoryId }: CategoryListProps
     }
   };
 
+  const AllCompaniesIcon = getCategoryIcon('전체');
+
   return (
-    <div>
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-center">
+        <div
+          className={`${isActive === '전체' ? 'bg-btn-blue-color' : 'bg-modal-background-color'} group flex w-full cursor-pointer items-center justify-center gap-2 rounded-xl px-[14px] py-[12px] transition-all duration-200 hover:bg-btn-blue-color`}
+          onClick={() => isActiveHandler('전체', 0)}
+        >
+          <div className="min-h-[25px] min-w-[25px]">
+            <AllCompaniesIcon />
+          </div>
+          <p
+            className={`${isActive === '전체' ? 'text-white' : 'text-border-color'} text-[16px] transition-all duration-200 group-hover:text-white`}
+          >
+            전체
+          </p>
+        </div>
+      </div>
       <div className="grid max-w-[660px] grid-cols-5 gap-[10px]">
-        {categoryNames.map((name, index) => {
+        {categoryNames.slice(1).map((name, index) => {
           const IconComponent = getCategoryIcon(name);
           return (
             <div
               className={`${isActive === name ? 'bg-btn-blue-color' : 'bg-modal-background-color'} group flex cursor-pointer items-center justify-center gap-2 rounded-xl px-[14px] py-[12px] transition-all duration-200 hover:bg-btn-blue-color`}
               key={index}
-              onClick={() => isActiveHandler(name, index)}
+              onClick={() => isActiveHandler(name, index + 1)}
             >
               <div className="min-h-[25px] min-w-[25px]">
                 <IconComponent />
