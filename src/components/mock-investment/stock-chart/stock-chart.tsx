1423e32530d148b093124efa844aaa23a56a1dc1
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-26 10:48:29 +0900

    design:대기 디자인 변경

--- src/components/mock-investment/stock-chart/stock-chart.tsx
+++ src/components/mock-investment/stock-chart/stock-chart.tsx
@@ -1,4 +1,5 @@
 import { TickData } from '@/api/types/stock';
+import { addCommasToThousand } from '@/utils/numberFormatter';
 
 // 인터페이스 정의
 interface TickInfoProps {
@@ -10,16 +11,22 @@ export const TickInfo = ({ tickData }: TickInfoProps) => {
     <div className="flex items-center gap-4">
       <div>
         {tickData === null ? (
-          <p>none</p>
+          <div></div>
         ) : (
           <div
             className={`duration-400 flex gap-3 rounded-2xl border-2 bg-opacity-40 p-3 px-6 transition-all ${tickData.ccldDvsn === '1' ? 'border-btn-red-color bg-btn-red-color bg-opacity-10 text-btn-red-color' : 'border-btn-blue-color bg-btn-blue-color  bg-opacity-10 text-btn-blue-color'}`}
           >
-            <h1>{tickData.stckPrpr}</h1>
+            <h1>{addCommasToThousand(tickData.stckPrpr)}</h1>
             <h1 className="text-white opacity-60">{tickData.cntgVol}</h1>
           </div>
         )}
-        {tickData === null ? <p>데이터를 기다리는 중...</p> : <ul></ul>}
+        {tickData === null ? (
+          <div className="rounded-2xl border-2 border-border-color bg-opacity-40 p-3 px-6">
+            <p>데이터를 기다리는 중...</p>
+          </div>
+        ) : (
+          <ul></ul>
+        )}
       </div>
       <p className="opacity-60">현재 거래</p>
     </div>
