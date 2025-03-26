8406fda4806593fe288184c0cce09cb8037e3eae
신해인 <godls0215@naver.com>
2025-03-26 16:34:52 +0900

    feat: 모의투자 및 튜토리얼 페이지 더미데이터 삭제

--- src/routes/pages/SimulatedInvestmentPage.tsx
+++ src/routes/pages/SimulatedInvestmentPage.tsx
@@ -1,11 +1,10 @@
 import ChartComponent from '@/components/ui/chart';
-import { dummyChartData } from '@/mocks/dummy-data';
 
 export const SimulatedInvestmentPage = () => {
   return (
     <div className="flex h-full w-full flex-col">
       <div className="flex-1">
-        <ChartComponent data={dummyChartData} height={600} />
+        <ChartComponent height={600} />
       </div>
     </div>
   );
