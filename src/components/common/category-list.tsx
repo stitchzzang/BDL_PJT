a20262b1a8b386d1fe9dc7205c57bb188237041f
frndhoon <frndhoon@gmail.com>
2025-03-26 14:00:37 +0900

    feat: category -> categoryId로 변경
    - param를 카테고리명이 아닌 카테고리 id로 받음

--- src/components/common/category-list.tsx
+++ src/components/common/category-list.tsx
@@ -4,20 +4,20 @@ import { type CategoryName, getCategoryIcon, getCategoryNames } from '@/utils/ca
 
 // props 선택
 export interface CategoryListProps {
-  setCategory: (category: string | '') => void;
+  setCategoryId: (category: string) => void;
 }
 
-export const CategoryList = ({ setCategory }: CategoryListProps) => {
+export const CategoryList = ({ setCategoryId: setCategoryId }: CategoryListProps) => {
   const categoryNames = getCategoryNames();
   const [isActive, setIsActive] = useState<CategoryName | ''>('');
 
-  const isActiveHandler = (name: CategoryName) => {
+  const isActiveHandler = (name: CategoryName, index: number) => {
     if (name === isActive) {
       setIsActive('');
-      setCategory('');
+      setCategoryId('0');
     } else {
       setIsActive(name);
-      setCategory(name);
+      setCategoryId(index.toString());
     }
   };
 
@@ -30,7 +30,7 @@ export const CategoryList = ({ setCategory }: CategoryListProps) => {
             <div
               className={`${isActive === name ? 'bg-btn-blue-color' : 'bg-modal-background-color'} group flex cursor-pointer items-center justify-center gap-2 rounded-xl px-[14px] py-[12px] transition-all duration-200 hover:bg-btn-blue-color`}
               key={index}
-              onClick={() => isActiveHandler(name)}
+              onClick={() => isActiveHandler(name, index)}
             >
               <div className="min-h-[25px] min-w-[25px]">
                 <IconComponent />
