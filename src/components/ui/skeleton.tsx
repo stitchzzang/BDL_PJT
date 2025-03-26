85faab34813fe7d7d9995fde4928406f168a28f7
frndhoon <frndhoon@gmail.com>
2025-03-26 10:36:57 +0900

    design: skeleton ui bg 색 변경

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,7 +1,15 @@
 import { cn } from '@/lib/utils';
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('animate-pulse rounded-md bg-primary/10', className)} {...props} />;
+  return (
+    <div
+      className={cn(
+        'animate-pulse rounded-md bg-point-color/30 [animation-duration:2s]',
+        className,
+      )}
+      {...props}
+    />
+  );
 }
 
 export { Skeleton };
