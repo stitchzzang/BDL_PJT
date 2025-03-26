61943878faf2ac5e28ba88629c4857613fc0c9c9
frndhoon <frndhoon@gmail.com>
2025-03-26 12:47:21 +0900

    feat: jwt decoding 로직 제거 - accessToken은 쿠키에 담아서 줌(로그인 확인용) - userData는 json으로 nickname, profile 사진을 줌

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,9 +1,6 @@
-import { jwtDecode } from 'jwt-decode';
 import { create } from 'zustand';
 
 interface UserData {
-  id: number | null;
-  email: string | null;
   nickname: string | null;
   profile: string | null;
 }
@@ -12,38 +9,31 @@ interface AuthState {
   userData: UserData;
   isLogin: boolean;
   isInitialized: boolean;
-  loginAuth: (token: string) => void;
+  loginAuth: (token: string, userData: UserData) => void;
   logoutAuth: () => void;
   initializeAuth: () => void;
 }
 
-type JWTPayload = UserData;
-
 export const useAuthStore = create<AuthState>((set) => ({
-  // userData만 전역 상태 관리
-  userData: { id: null, email: null, nickname: null, profile: null }, // const {id, email, nickname} = useAuthStore((state) => state.userData) 형식으로 사용
+  userData: { nickname: null, profile: null },
   isLogin: false,
   isInitialized: false,
 
-  // 토큰 저장 및 userData 업데이트
-  loginAuth: (token: string) => {
-    const decoded = jwtDecode<JWTPayload>(token);
+  loginAuth: (token: string, userData: UserData) => {
     localStorage.setItem('accessToken', token);
-    set({ userData: decoded, isLogin: true });
+    set({ userData, isLogin: true });
   },
 
-  // 토큰 제거 및 userData 초기화
   logoutAuth: () => {
     localStorage.removeItem('accessToken');
-    set({ userData: { id: null, email: null, nickname: null, profile: null }, isLogin: false });
+    set({ userData: { nickname: null, profile: null }, isLogin: false });
   },
 
   // 토큰 및 userData 초기화 함수 (App.jsx에서 호출 됨에 따라 앱 마운트가 될 때 토큰으로 userData 업데이트)
   initializeAuth: () => {
     const token = localStorage.getItem('accessToken');
     if (token) {
-      const decoded = jwtDecode<JWTPayload>(token);
-      set({ userData: decoded, isLogin: true, isInitialized: true });
+      set({ isLogin: true, isInitialized: true });
     } else {
       set({ isInitialized: true });
     }
