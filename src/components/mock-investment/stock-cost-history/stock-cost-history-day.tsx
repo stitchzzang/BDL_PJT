fedd6f96515633a7748a14541f21b20b4af2f9c6
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-26 11:14:49 +0900

    feat: 상단 당일 값 실시간 으로 데이터 변경 가능하게 수정

--- src/components/mock-investment/stock-cost-history/stock-cost-history-day.tsx
+++ src/components/mock-investment/stock-cost-history/stock-cost-history-day.tsx
@@ -1,10 +1,13 @@
+import { TickData } from '@/api/types/stock';
 import { DTData } from '@/components/mock-investment/stock-cost-history/stock-cost-history';
+import { getTodayFormatted } from '@/utils/getTodayFormatted';
 import { formatKoreanMoney } from '@/utils/numberFormatter';
 
 interface StockCostHistoryDayProps {
   dayDataList: DTData[];
+  tickData: TickData | null;
 }
-export const StockCostHistoryDay = ({ dayDataList }: StockCostHistoryDayProps) => {
+export const StockCostHistoryDay = ({ dayDataList, tickData }: StockCostHistoryDayProps) => {
   return (
     <div>
       <div className="w-full">
@@ -22,7 +25,31 @@ export const StockCostHistoryDay = ({ dayDataList }: StockCostHistoryDayProps) =
               <div className="w-[20%] text-right text-[16px] text-border-color">고가</div>
               <div className="w-[20%] text-right text-[16px] text-border-color">저가</div>
             </div>
-
+            {/* 가장 상단은 항상 최신신 */}
+            <div className="flex flex-row rounded-lg bg-[#102038] p-3 text-white hover:bg-modal-background-color">
+              <div className="w-[20%] text-[14px] font-light text-border-color">
+                {getTodayFormatted()}
+              </div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">
+                {tickData ? formatKoreanMoney(tickData.stckPrpr) : ''}
+              </div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">등락률</div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">
+                {tickData ? formatKoreanMoney(tickData.acmlVol) : ''}
+              </div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">
+                {tickData ? formatKoreanMoney(tickData.acmlTrPbm) : ''}
+              </div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">
+                {tickData ? formatKoreanMoney(tickData.stckOprc) : ''}
+              </div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">
+                {tickData ? formatKoreanMoney(tickData.stckHgpr) : ''}
+              </div>
+              <div className="w-[20%] text-right text-[16px] text-border-color">
+                {tickData ? formatKoreanMoney(tickData.stckLwpr) : ''}
+              </div>
+            </div>
             {/* 테이블 로우들 - 배열의 각 항목을 매핑 */}
             {dayDataList.map((item, index) => (
               <div
