c3edcdd85081f380e60e4e9ebcce34be48f4961d
frndhoon <frndhoon@gmail.com>
2025-03-27 10:59:14 +0900

    feat: 프로필 컴포넌트를 useAuthStore를 통해 사용자 데이터로 업데이트

--- src/components/member-info/profile.tsx
+++ src/components/member-info/profile.tsx
@@ -1,59 +1,19 @@
 import { Cog6ToothIcon } from '@heroicons/react/24/outline';
 import { NavLink } from 'react-router-dom';
 
-import { type MemberInfo } from '@/api/types/member';
-import { LoadingSpinner } from '@/components/ui/loading-spinner';
+import { useAuthStore } from '@/store/useAuthStore';
 
-interface ProfileProps {
-  memberInfo?: MemberInfo;
-  isLoading?: boolean;
-  isError?: boolean;
-}
-
-export const Profile = ({ memberInfo, isLoading, isError }: ProfileProps) => {
-  if (isLoading) {
-    return (
-      <div className="flex flex-col items-center gap-2">
-        <LoadingSpinner />
-      </div>
-    );
-  }
-
-  if (isError) {
-    return (
-      <div className="flex flex-col items-center gap-2">
-        <img
-          src="/none-img/none_profile_img.png"
-          alt="profile"
-          className="h-10 w-10 rounded-full"
-        />
-        <p className="text-2xl font-medium text-btn-red-color">프로필을 불러오는데 실패했습니다.</p>
-
-        <NavLink
-          to="/member/edit"
-          className={({ isActive }) =>
-            `flex items-center gap-1 rounded-lg px-4 py-2 text-text-inactive-3-color transition-colors ${
-              isActive
-                ? 'bg-modal-background-color text-text-main-color'
-                : 'hover:bg-modal-background-color hover:text-text-main-color'
-            }`
-          }
-        >
-          <Cog6ToothIcon className="h-5 w-5" />
-          <p className="text-sm">프로필 수정</p>
-        </NavLink>
-      </div>
-    );
-  }
+export const Profile = () => {
+  const { userData } = useAuthStore();
 
   return (
     <div className="flex flex-col items-center gap-2">
       <img
-        src={memberInfo?.profileImage || '/none-img/none_profile_img.png'}
+        src={userData.profile || '/none-img/none_profile_img.png'}
         alt="profile"
         className="h-10 w-10 rounded-full"
       />
-      <p className="text-2xl font-medium">{memberInfo?.nickname || '현재 닉네임이 없습니다.'}</p>
+      <p className="text-2xl font-medium">{userData.nickname || '현재 닉네임이 없습니다.'}</p>
       <NavLink
         to="/member/edit"
         className={({ isActive }) =>
