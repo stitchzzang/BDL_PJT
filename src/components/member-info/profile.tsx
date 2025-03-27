4c08011a082754e3cb16d33ab5507681b6ac3e12
frndhoon <frndhoon@gmail.com>
2025-03-27 10:10:02 +0900

    feat: 프로필 수정 버튼 추가(error시)

--- src/components/member-info/profile.tsx
+++ src/components/member-info/profile.tsx
@@ -28,6 +28,20 @@ export const Profile = ({ memberInfo, isLoading, isError }: ProfileProps) => {
           className="h-10 w-10 rounded-full"
         />
         <p className="text-2xl font-medium text-btn-red-color">프로필을 불러오는데 실패했습니다.</p>
+
+        <NavLink
+          to="/member/edit"
+          className={({ isActive }) =>
+            `flex items-center gap-1 rounded-lg px-4 py-2 text-text-inactive-3-color transition-colors ${
+              isActive
+                ? 'bg-modal-background-color text-text-main-color'
+                : 'hover:bg-modal-background-color hover:text-text-main-color'
+            }`
+          }
+        >
+          <Cog6ToothIcon className="h-5 w-5" />
+          <p className="text-sm">프로필 수정</p>
+        </NavLink>
       </div>
     );
   }
