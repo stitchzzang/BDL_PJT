9b01ceaf0300409ffff63b5b88b770686ec4f109
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-27 17:41:56 +0900

    test:테스트용 - 추후 변경

--- src/components/mock-investment/order-status/order-status-wait-list.tsx
+++ src/components/mock-investment/order-status/order-status-wait-list.tsx
@@ -64,50 +64,65 @@ export const OrderStatusWaitList = ({ UserSimulatedData }: OrderStatusWaitListPr
   };
   // 클릭시 반응형 추가
   const [isActive, setIsActive] = useState<boolean>(false);
+  const [editor, setEditor] = useState<boolean>(true);
   const isActiveHandler = () => {
     setIsActive(!isActive);
   };
   return (
-    <div
-      onClick={() => isActiveHandler()}
-      className={`mb-3 mt-[12px] flex flex-col gap-4  rounded-xl p-3 ${UserSimulatedData.tradeType === 0 ? 'bg-btn-red-color bg-opacity-20' : 'bg-btn-blue-color bg-opacity-20'}`}
-    >
-      <div className={`flex justify-between`}>
-        <div className="flex items-center gap-3">
-          <h3 className={h3Style}>{UserSimulatedData.companyName}</h3>
-          <p className="text-border-color">
-            {formatKoreanMoney(UserSimulatedData.price)}원 <span>|</span>{' '}
-            {UserSimulatedData.quantity}주
-          </p>
-        </div>
-        <div className="flex gap-2">
-          <div>
-            {UserSimulatedData.tradeType === 1 ? (
-              <p className="text-btn-blue-color">판매</p>
-            ) : (
-              <p className="text-btn-red-color ">구매</p>
-            )}
+    <>
+      {editor ? (
+        <div
+          onClick={() => isActiveHandler()}
+          className={`mb-3 mt-[12px] flex flex-col gap-4  rounded-xl p-3 ${UserSimulatedData.tradeType === 0 ? 'bg-btn-red-color bg-opacity-20' : 'bg-btn-blue-color bg-opacity-20'}`}
+        >
+          <div className={`flex justify-between`}>
+            <div className="flex items-center gap-3">
+              <h3 className={h3Style}>{UserSimulatedData.companyName}</h3>
+              <p className="text-border-color">
+                {formatKoreanMoney(UserSimulatedData.price)}원 <span>|</span>{' '}
+                {UserSimulatedData.quantity}주
+              </p>
+            </div>
+            <div className="flex gap-2">
+              <div>
+                {UserSimulatedData.tradeType === 1 ? (
+                  <p className="text-btn-blue-color">판매</p>
+                ) : (
+                  <p className="text-btn-red-color ">구매</p>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      {isActive ? (
-        <div className="flex gap-2">
-          <Button variant="green" className="w-full" size="sm">
-            <p>수정하기</p>
-          </Button>
-          <Button
-            variant="red"
-            className="w-full"
-            size="sm"
-            onClick={() => handleDeleteSimulatedMutation(UserSimulatedData.orderId)}
-          >
-            <p>취소하기</p>
-          </Button>
+          {isActive ? (
+            <div className="flex gap-2">
+              <Button
+                variant="green"
+                className="w-full"
+                size="sm"
+                onClick={() => setEditor(!editor)}
+              >
+                <p>수정하기</p>
+              </Button>
+              <Button
+                variant="red"
+                className="w-full"
+                size="sm"
+                onClick={() => handleDeleteSimulatedMutation(UserSimulatedData.orderId)}
+              >
+                <p>삭제하기</p>
+              </Button>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       ) : (
-        <></>
+        // <OrderStatusEditor />
+        <div>
+          <p>test</p>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
