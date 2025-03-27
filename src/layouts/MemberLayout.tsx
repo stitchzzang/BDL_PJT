1bf47febf8c980bdf9ae1a5e20aeaf1e666993fb
frndhoon <frndhoon@gmail.com>
2025-03-27 11:10:05 +0900

    feat: member layout에서 불필요한 member 정보 api 호출 제거

--- src/layouts/MemberLayout.tsx
+++ src/layouts/MemberLayout.tsx
@@ -1,16 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
-import { useMemberInfo } from '@/api/member.api';
 import { Profile } from '@/components/member-info/profile';
 import { SectionNavBar } from '@/components/member-info/section-nav-bar';
 
 export const MemberLayout = () => {
-  const { data: memberInfo, isLoading, isError } = useMemberInfo({ memberId: '1' });
-
   return (
     <>
       <div className="flex flex-col items-center gap-4">
-        <Profile memberInfo={memberInfo} isLoading={isLoading} isError={isError} />
+        <Profile />
         <SectionNavBar />
       </div>
       <hr className="mb-12 mt-3 border-t border-btn-primary-inactive-color" />
