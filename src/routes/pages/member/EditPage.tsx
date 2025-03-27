cf9a2ddf91b716b6497146b3ec899db92b9c55c6
frndhoon <frndhoon@gmail.com>
2025-03-27 10:18:05 +0900

    feat: 회원 정보 수정 api 적용

--- src/routes/pages/member/EditPage.tsx
+++ src/routes/pages/member/EditPage.tsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { useUpdateMemberInfo } from '@/api/member.api';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-
 export const EditPage = () => {
   const navigate = useNavigate();
+
+  const [nickname, setNickname] = useState(''); // 추후 useAuthStore에서 가져오기
+  const [profileImage, setProfileImage] = useState(''); // 추후 useAuthStore에서 가져오기
+
+  const { mutate: updateMemberInfo, isPending: updatePending } = useUpdateMemberInfo({
+    memberId: '1', // 추후 useAuthStore에서 가져오기
+    data: {
+      nickname,
+      profileImage,
+    },
+  });
   return (
     <div className="flex flex-col items-center gap-4">
       <h1 className="text-2xl font-bold">프로필 수정</h1>
       <div className="flex min-w-[400px] flex-col items-center gap-5 rounded-lg border border-btn-primary-inactive-color bg-modal-background-color p-4">
         <div className="flex flex-col items-center gap-4">
           <img
-            src="/none-img/none_profile_img.png"
+            src={profileImage || '/none-img/none_profile_img.png'}
             alt="profile"
             className="h-32 w-32 rounded-full"
           />
@@ -20,7 +32,13 @@ export const EditPage = () => {
           </Button>
         </div>
         <div className="flex w-full flex-col items-center gap-2">
-          <Input type="text" className="h-14" placeholder="이름" />
+          <Input
+            type="text"
+            className="h-14"
+            placeholder="이름"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
           <p className="text-sm text-text-inactive-2-color">변경할 닉네임을 입력해주세요.</p>
         </div>
         <div className="flex w-full flex-col gap-4">
@@ -31,7 +49,7 @@ export const EditPage = () => {
           >
             비밀번호 변경
           </Button>
-          <Button variant="blue" className="w-full">
+          <Button variant="blue" className="w-full" onClick={() => updateMemberInfo()}>
             프로필 수정
           </Button>
         </div>
