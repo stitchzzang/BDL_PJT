5fd8d6731c8a4fd8a6040f2899bc995a7f76377e
dnwnsrb11 <dnwnsrb11@naver.com>
2025-03-28 10:49:37 +0900

    feat:데이터 없을 경우 애니메이션 추가

--- src/components/mock-investment/selling-price/selling-price.tsx
+++ src/components/mock-investment/selling-price/selling-price.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { OrderbookData } from '@/api/types/stock';
+import { ChartLoadingAnimation } from '@/components/common/chart-loading-animation';
 import { SellingPriceSell } from '@/components/mock-investment/selling-price/selling-price-sell';
 import { useOrderbookConnection } from '@/services/SocketStockOrderbookDataService';
 
@@ -32,22 +33,36 @@ const orderBookDummy: orderBookData = {
 export const SellingPrice = () => {
   // 소켓 연결
   const { IsConnected, connectOrderbook, disconnectOrderbook } = useOrderbookConnection();
-  const [tickData, setTickData] = useState<OrderbookData | null>(null);
+  const [orderbookData, setOrderbookData] = useState<OrderbookData | null>(null);
+
+  useEffect(() => {
+    // 소켓 연결
+    connectOrderbook('000660', setOrderbookData);
+    return () => {
+      disconnectOrderbook();
+    };
+  }, [connectOrderbook, disconnectOrderbook]);
   return (
     <div className="h-full rounded-2xl bg-modal-background-color p-[20px]">
-      <div className="flex justify-between">
-        <h3 className="text-[18px] font-bold">호가</h3>
-        {/* 장시간 string */}
-        <p className="text-[14px] font-light text-border-color">12:32</p>
-      </div>
-      <div className="mt-[20px] flex justify-between">
-        <p className="text-[18px] text-btn-blue-color">매도잔량</p>
-        <p className="text-[18px] text-btn-red-color">매수잔량</p>
-      </div>
-      <hr className="mt-[16px] border-border-color border-opacity-20" />
-      <div>
-        <SellingPriceSell orderBookDummy={orderBookDummy} />
-      </div>
+      {orderbookData ? (
+        <>
+          <div className="flex justify-between">
+            <h3 className="text-[18px] font-bold">호가</h3>
+            {/* 장시간 string */}
+            <p className="text-[14px] font-light text-border-color">12:32</p>
+          </div>
+          <div className="mt-[20px] flex justify-between">
+            <p className="text-[18px] text-btn-blue-color">매도잔량</p>
+            <p className="text-[18px] text-btn-red-color">매수잔량</p>
+          </div>
+          <hr className="mt-[16px] border-border-color border-opacity-20" />
+          <div>
+            <SellingPriceSell orderBookDummy={orderBookDummy} />
+          </div>
+        </>
+      ) : (
+        <ChartLoadingAnimation />
+      )}
     </div>
   );
 };
