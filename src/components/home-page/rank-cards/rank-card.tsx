4dcd30304590f16f57c60a9970184880608bcb0f
frndhoon <frndhoon@gmail.com>
2025-03-28 10:23:50 +0900

    feat: user Ranking api 연동

--- src/components/home-page/rank-cards/rank-card.tsx
+++ src/components/home-page/rank-cards/rank-card.tsx
@@ -1,24 +1,24 @@
-import { RankInformation } from '@/components/home-page/rank-cards/rank-cards';
+import { UserRanking } from '@/api/types/home';
 
 interface RankCardProps {
-  rankInfo: RankInformation;
+  rankInfo: UserRanking;
 }
 
 export const RankCard = ({ rankInfo }: RankCardProps) => {
-  const { name, rate, imgUrl } = rankInfo;
+  const { nickname, profile, changeRate } = rankInfo;
   return (
     <div className="flex items-center justify-center rounded-2xl border border-background-color bg-modal-background-color p-[30px] transition-all duration-300 hover:border-btn-blue-color">
       <div className="flex flex-col items-center justify-center gap-[15px]">
-        {imgUrl === null ? (
+        {profile === null ? (
           <div className="max-h-[168px] max-w-[168px] overflow-hidden rounded-2xl">
             <img src="/none-img/none_profile_img.png" alt="noneImg" />
           </div>
         ) : (
-          <img src={imgUrl} alt="profileImg" />
+          <img src={profile} alt="profileImg" />
         )}
-        <p className="text-[22px] font-medium">{name}</p>
+        <p className="text-[22px] font-medium">{nickname}</p>
         <div className="rounded-2xl border border-btn-red-color px-[28px] py-[12px]">
-          <p className="text-[28px] font-bold text-btn-red-color">{rate} %</p>
+          <p className="text-[28px] font-bold text-btn-red-color">{changeRate} %</p>
         </div>
       </div>
     </div>
