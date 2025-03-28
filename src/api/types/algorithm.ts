be510968f4b466a52a12e8dc0390c849667636e9
frndhoon <frndhoon@gmail.com>
2025-03-28 11:13:39 +0900

    type: 알고리즘 인터페이스 구조 변경 및 실행 중인 회사 정보 추가

--- src/api/types/algorithm.ts
+++ src/api/types/algorithm.ts
@@ -1,27 +1,36 @@
+export interface RunningCompany {
+  companyId: number;
+  companyName: string;
+}
+
 export interface Algorithm {
-  algorithmId?: number; // 알고리즘 이름
-  algorithmName: string; // 수익률(%)이 이 값에 도달하면 매도
-  entryMethod: 'ONCE' | 'DIVIDE'; // 매수 방법
-  exitMethod: 'ONCE' | 'DIVIDE'; // 매도 방법
-  profitPercentToSell?: number; // 수익률(%)이 이 값에 도달하면 매도
-  lossPercentToSell?: number; // 손실률(%)이 이 값에 도달하면 매도
-  oneMinuteIncreasePercent?: number; // 1분 증가 퍼센트
-  oneMinuteIncreaseAction?: 'BUY' | 'SELL'; // 1분 증가 행동
-  oneMinuteDecreasePercent?: number; // 1분 감소 퍼센트
-  oneMinuteDecreaseAction?: 'BUY' | 'SELL'; // 1분 감소 행동
-  dailyIncreasePercent?: number; // 일 증가 퍼센트
-  dailyIncreaseAction?: 'BUY' | 'SELL'; // 일 증가 행동
-  dailyDecreasePercent?: number; // 일 감소 퍼센트
-  dailyDecreaseAction?: 'BUY' | 'SELL'; // 일 감소 행동
-  shortTermMaPeriod?: number; // 단기 이동 평균 기간
-  longTermMaPeriod?: number; // 장기 이동 평균 기간
-  isFee?: boolean; // 수수료 여부
-  entryInvestmentMethod: 'FIXED_AMOUNT' | 'FIXED_PERCENTAGE'; // 매수 투자 방법
-  entryFixedAmount?: number; // 매수 투자 금액
-  entryFixedPercentage: number; // 매수 투자 퍼센트
-  exitInvestmentMethod?: 'FIXED_AMOUNT' | 'FIXED_PERCENTAGE'; // 매도 투자 방법
-  exitFixedAmount?: number; // 매도 투자 금액
-  exitFixedPercentage?: number; // 매도 투자 퍼센트
+  algorithmId: number;
+  algorithmName: string;
+  profitPercentToSell: number;
+  lossPercentToSell: number;
+  oneMinuteIncreasePercent: number | null;
+  oneMinuteIncreaseAction: string | null;
+  oneMinuteDecreasePercent: number | null;
+  oneMinuteDecreaseAction: string | null;
+  dailyIncreasePercent: number | null;
+  dailyIncreaseAction: string | null;
+  dailyDecreasePercent: number | null;
+  dailyDecreaseAction: string | null;
+  shortTermMaPeriod: number | null;
+  longTermMaPeriod: number | null;
+  entryMethod: string;
+  entryInvestmentMethod: string;
+  entryFixedAmount: number | null;
+  entryFixedPercentage: number | null;
+  exitMethod: string;
+  exitInvestmentMethod: string | null;
+  exitFixedAmount: number | null;
+  exitFixedPercentage: number | null;
+  isFee: boolean;
+  createdAt: string;
+  updatedAt: string;
+  isRunning: boolean;
+  runningCompanies: RunningCompany[];
 }
 
 export interface AlgorithmResponse {
