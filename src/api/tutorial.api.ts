d447a11ea2b80bdd232f6e8ce67f54ec6dbc3d0a
woojeonghoon <hoonixox@naver.com>
2025-03-30 14:15:58 +0900

    type: tutorial 결과 type dto에 맞게 수정

--- src/api/tutorial.api.ts
+++ src/api/tutorial.api.ts
@@ -7,7 +7,7 @@ import { TutorialResultResponse } from '@/api/types/tutorial';
 // 튜토리얼 관련 api (https://www.notion.so/otterbit/API-1a42f79c753081d38d42cf8c22a01fa3?pvs=4)
 export const tutorialApi = {
   getTutorialResults: ({ memberId }: { memberId: string }) =>
-    _ky.get<ApiResponse<TutorialResultResponse>>(`tutorial/result/${memberId}`).json(),
+    _ky.get<ApiResponse<TutorialResultResponse[]>>(`tutorial/result/${memberId}`).json(),
 };
 
 export const useTutorialResults = ({ memberId }: { memberId: string }) => {
--- src/api/types/tutorial.ts
+++ src/api/types/tutorial.ts
@@ -1,15 +1,11 @@
 export interface TutorialResultResponse {
-  tutorialResult: TutorialResult[];
-}
-
-export interface TutorialResult {
-  tutorialResultId: number; // 튜토리얼 결과 ID
-  companyId: number; // 기업 ID
-  companyName: string; // 기업명
-  startMoney: number; // 시작 금액
-  endMoney: number; // 최종 금액
-  startDate: string; // 시작날짜
-  endDate: string; // 종료날짜
-  memberId: number; // 회원 ID
-  memberName: string; // 회원 이름
+  tutorialResultId: number;
+  companyId: number;
+  companyName: string;
+  startMoney: number;
+  endMoney: number;
+  startDate: string;
+  endDate: string;
+  memberId: number;
+  memberName: string;
 }
