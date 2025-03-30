4f57b88f49859ee5267188c76335b1576acfb685
woojeonghoon <hoonixox@naver.com>
2025-03-30 16:32:21 +0900

    design: 주식 튜토리얼 결과 hover 시 배경색 변경

--- src/components/member-info/stock-tutorial-result-item.tsx
+++ src/components/member-info/stock-tutorial-result-item.tsx
@@ -12,7 +12,7 @@ interface StockTutorialResultItemProps {
 
 export const StockTutorialResultItem = ({ result }: StockTutorialResultItemProps) => {
   return (
-    <div className="flex w-full flex-row items-center justify-between gap-2 rounded-[10px] bg-modal-background-color p-3">
+    <div className="flex w-full flex-row items-center justify-between gap-2 rounded-[10px] bg-modal-background-color p-3 hover:bg-modal-background-color/50">
       <div className="flex flex-row items-center gap-4">
         <img
           src="https://placehold.co/50x50"
