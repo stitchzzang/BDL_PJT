2e6644d3d31181ea2d57552c21073e4877818285
frndhoon <frndhoon@gmail.com>
2025-03-31 13:41:19 +0900

    feat: 사용자 데이터 로컬 스토리지에서 가져오도록 authStore 수정 및 업데이트 시 동기화 추가

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -15,34 +15,44 @@ interface AuthState {
   updateAuth: (userData: Partial<UserData>) => void;
 }
 
+const getUserDataFromStorage = (): UserData => {
+  const storedData = localStorage.getItem('userData');
+  return storedData ? JSON.parse(storedData) : { nickname: null, profile: null };
+};
+
 export const useAuthStore = create<AuthState>((set) => ({
-  userData: { nickname: null, profile: null },
-  isLogin: false,
+  userData: getUserDataFromStorage(),
+  isLogin: getUserDataFromStorage().nickname !== null,
   isInitialized: false,
 
   loginAuth: (token: string, userData: UserData) => {
     localStorage.setItem('accessToken', token);
+    localStorage.setItem('userData', JSON.stringify(userData));
     set({ userData, isLogin: true });
   },
 
   logoutAuth: () => {
     localStorage.removeItem('accessToken');
+    localStorage.removeItem('userData');
     set({ userData: { nickname: null, profile: null }, isLogin: false });
   },
 
   updateAuth: (newUserData: Partial<UserData>) => {
-    set((state) => ({
-      userData: { ...state.userData, ...newUserData },
-    }));
+    set((state) => {
+      const updatedUserData = { ...state.userData, ...newUserData };
+      localStorage.setItem('userData', JSON.stringify(updatedUserData));
+      return { userData: updatedUserData };
+    });
   },
 
   // 토큰 및 userData 초기화 함수 (App.jsx에서 호출 됨에 따라 앱 마운트가 될 때 토큰으로 userData 업데이트)
   initializeAuth: () => {
     const token = localStorage.getItem('accessToken');
+    const userData = getUserDataFromStorage();
     if (token) {
-      set({ isLogin: true, isInitialized: true });
+      set({ isLogin: true, isInitialized: true, userData });
     } else {
-      set({ isInitialized: true });
+      set({ isInitialized: true, userData });
     }
   },
 }));
