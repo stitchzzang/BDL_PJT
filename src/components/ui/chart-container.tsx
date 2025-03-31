fcccf8a150e9f0d4e8d79b7bfccbd994c6b51817
WJG <dnwnsrb11@naver.com>
2025-03-31 20:24:26 +0900

    feat:컨테이너 인터페이스 정의

--- src/components/ui/chart-container.tsx
+++ src/components/ui/chart-container.tsx
@@ -1,6 +1,52 @@
+import { DailyChart } from '@/components/ui/chart-daily';
 import { MinuteChart } from '@/components/ui/chart-simulate';
 
-export const ChartConstainer = () => {
+// 타입 정의 (분봉데이터 - 초기 데이터 적재를 위하여)
+interface StockMinuteData {
+  stockCandleMinuteId: number;
+  companyId: string;
+  openPrice: number;
+  openPricePercent: number;
+  highPrice: number;
+  highPricePercent: number;
+  lowPrice: number;
+  lowPricePercent: number;
+  closePrice: number;
+  closePricePercent: number;
+  contractingVolume: number;
+  accumulatedTradeAmount: number;
+  tradingTime: string;
+  fiveAverage: number;
+  twentyAverage: number;
+}
+
+interface StockMinuteDefaultData {
+  companyId: string;
+  limit: number;
+  cursor: string;
+  data: StockMinuteData[];
+}
+
+// 차트 데이터 포인트 타입
+// interface ChartDataPoint {
+//   date: string; // 날짜 표시용
+//   open: number;
+//   high: number;
+//   low: number;
+//   close: number;
+//   volume: number;
+//   changeType: 'RISE' | 'FALL' | 'NONE';
+//   fiveAverage: number;
+//   twentyAverage: number;
+//   rawDate: Date | null;
+// }
+
+interface MinuteChartProps {
+  companyId?: string;
+  initialData?: StockMinuteDefaultData; // 부모 컴포넌트에서 받는 초기 데이터
+}
+
+export const ChartConstainer = ({ initialData }: MinuteChartProps) => {
   return (
     <div>
       <div>
@@ -9,7 +55,10 @@ export const ChartConstainer = () => {
         <button>3</button>
       </div>
       <div>
-        <MinuteChart />
+        <MinuteChart initialData={initialData} />
+      </div>
+      <div>
+        <DailyChart periodType={'day'} />
       </div>
     </div>
   );
