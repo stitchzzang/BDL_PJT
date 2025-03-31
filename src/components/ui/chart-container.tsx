f9b8f88ed21d17e62efc5f5ea966ae2c22944e23
WJG <dnwnsrb11@naver.com>
2025-03-31 19:39:35 +0900

    feat:차트 콘테이너 기초 구조화

--- src/components/ui/chart-container.tsx
+++ src/components/ui/chart-container.tsx
@@ -1,7 +1,16 @@
+import { MinuteChart } from '@/components/ui/chart-simulate';
+
 export const ChartConstainer = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+      <div>
+        <MinuteChart />
+      </div>
     </div>
   );
 };
