f2db25a3815ea4742fc0071373e7a2d448c8472c
frndhoon <frndhoon@gmail.com>
2025-04-01 14:26:55 +0900

    feat: member api 에러 핸들러 처리

--- src/api/member.api.ts
+++ src/api/member.api.ts
@@ -1,11 +1,13 @@
 // 회원 관련 api (https://www.notion.so/otterbit/API-1a42f79c753081d38d42cf8c22a01fa3?pvs=4)
 
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { HTTPError } from 'ky';
+import { toast } from 'react-hot-toast';
 
 import { _ky } from '@/api/instance';
+import { handleKyError } from '@/api/instance/errorHandler';
 import { ApiResponse } from '@/api/types/common';
 import { AccountSummaryResponse, MemberInfo, MemberPassword } from '@/api/types/member';
-
 export const memberApi = {
   getMemberInfo: (memberId: string) =>
     _ky.get<ApiResponse<MemberInfo>>(`member/${memberId}`).json(),
@@ -49,11 +51,11 @@ export const useUpdateMemberInfo = ({
     onSuccess: () => {
       // 프로필 정보 갱신
       queryClient.invalidateQueries({ queryKey: ['memberInfo', memberId] });
-      alert('프로필이 성공적으로 업데이트되었습니다.');
+      toast.success('프로필이 성공적으로 업데이트되었습니다.');
       onSuccess?.();
     },
-    onError: () => {
-      alert('프로필 업데이트에 실패했습니다. 다시 시도해주세요.');
+    onError: (error: HTTPError) => {
+      handleKyError(error, '프로필 업데이트에 실패했습니다. 다시 시도해주세요.');
       onError?.();
     },
   });
@@ -73,11 +75,11 @@ export const useUpdateMemberPassword = ({
   return useMutation({
     mutationFn: () => memberApi.updateMemberPassword(memberId, data),
     onSuccess: () => {
-      alert('비밀번호가 성공적으로 업데이트되었습니다.');
+      toast.success('비밀번호가 성공적으로 업데이트되었습니다.');
       onSuccess?.();
     },
-    onError: () => {
-      alert('비밀번호 업데이트에 실패했습니다. 다시 시도해주세요.');
+    onError: (error: HTTPError) => {
+      handleKyError(error, '비밀번호 업데이트에 실패했습니다. 다시 시도해주세요.');
       onError?.();
     },
   });
@@ -94,11 +96,11 @@ export const useResetAccount = (memberId: string, onSuccess?: () => void, onErro
   return useMutation({
     mutationFn: () => memberApi.resetAccount(memberId),
     onSuccess: () => {
-      alert('계좌가 성공적으로 초기화되었습니다.');
+      toast.success('계좌가 성공적으로 초기화되었습니다.');
       onSuccess?.();
     },
-    onError: () => {
-      alert('계좌 초기화에 실패했습니다. 다시 시도해주세요.');
+    onError: (error: HTTPError) => {
+      handleKyError(error, '계좌 초기화에 실패했습니다. 다시 시도해주세요.');
       onError?.();
     },
   });
