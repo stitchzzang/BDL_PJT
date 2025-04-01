cde6778364000f8a372a6b917df1bd7d5a5f53c9
frndhoon <frndhoon@gmail.com>
2025-04-01 16:46:54 +0900

    feat: 여러 레이아웃에 페이지 이동 시 스크롤을 상단으로 이동하는 훅 적용

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,11 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 import { Footer } from '@/components/common/footer';
 import { NavBar } from '@/components/common/nav-bar';
+import { useScrollTopOnRouteChange } from '@/hooks/useScrollTopOnRouteChange';
 import { useResetAlgorithmLabStore } from '@/store/useAlgorithmLabStore';
 
 export const MainLayout = () => {
   // URL 변경 시 알고리즘 랩 상태 초기화
   useResetAlgorithmLabStore();
+  // 페이지 이동 시마다 스크롤을 상단으로 이동
+  useScrollTopOnRouteChange();
 
   return (
     <div className="flex min-h-screen flex-col">
