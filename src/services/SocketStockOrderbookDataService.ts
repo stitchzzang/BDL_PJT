13770080c7efaaeabf14462afccbf5896ea27938
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-01 13:00:28 +0900

    feat:소켓 배포서버로 변경

--- src/services/SocketStockOrderbookDataService.ts
+++ src/services/SocketStockOrderbookDataService.ts
@@ -18,7 +18,7 @@ export const useOrderbookConnection = () => {
       //인스턴스 생성
       //https://j12d202.p.ssafy.io/ws
       //http://192.168.100.198:8080/ws
-      const socket = new SockJS('http://192.168.100.198:8080/ws');
+      const socket = new SockJS('https://j12d202.p.ssafy.io/ws');
 
       // STOMP 클라이언트 생성
       const client = new Client({
