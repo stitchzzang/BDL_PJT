15254b313f6ee0f0d76e0a8b0ee96aa30f6ef354
frndhoon <frndhoon@gmail.com>
2025-04-01 14:58:41 +0900

    type: member 정보 옵션으로 변경

--- src/api/types/member.ts
+++ src/api/types/member.ts
@@ -1,6 +1,7 @@
 export interface MemberInfo {
-  profileImage: string; // 프로필 이미지
-  nickname: string; // 닉네임
+  profileImage?: string; // 프로필 이미지
+  nickname?: string; // 닉네임
+  deleteProfile?: boolean; // 프로필 삭제 여부
 }
 
 export interface MemberPassword {
