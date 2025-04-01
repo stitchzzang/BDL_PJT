13600b9fcf8573947d28cf44977c8c8c94c3326d
woojeonghoon <hoonixox@naver.com>
2025-04-01 21:24:37 +0900

    chore: react-query dev tools 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 
 import { queryClient } from '@/lib/queryClient';
@@ -9,6 +10,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <AppRouter />
       <Toaster position="bottom-center" />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
