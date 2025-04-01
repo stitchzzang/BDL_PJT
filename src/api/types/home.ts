b40a3b56ecb2d36a6fb10f5b6a7e78ced6f491cb
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-01 12:48:07 +0900

    feat:홈에 들어갈 코시피,코스닥 정보 정의

--- src/api/types/home.ts
+++ src/api/types/home.ts
@@ -28,3 +28,15 @@ export interface UserRanking {
 export interface UserRankingResponse {
   userRanking: UserRanking[] | null;
 }
+
+// 코스피,코스닥 인터페이스
+export interface HomeChartData {
+  bstpNmixPrpr: string; //현재가
+  acmlVol: string; //거래량
+  bstpNmixOprc: string; //시가
+  bstpNmixHgpr: string; //고가
+  bstpNmixLwpr: string; //저가
+  stckBsopDate: string; //기준일
+  bstpNmixPrdyVrss: string; //전일 대비 변화량
+  bstpNmixPrdyCtrt: string; //전일 대비 변동률
+}
