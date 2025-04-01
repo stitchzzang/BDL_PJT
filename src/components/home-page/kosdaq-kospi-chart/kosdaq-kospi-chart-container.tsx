9ad3a4d6713150c6e9a8f9dbbc70ef5551fafe66
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-01 13:00:10 +0900

    feat:코스닥 차트 배치

--- src/components/home-page/kosdaq-kospi-chart/kosdaq-kospi-chart-container.tsx
+++ src/components/home-page/kosdaq-kospi-chart/kosdaq-kospi-chart-container.tsx
@@ -1,7 +1,11 @@
+import MainKosdaqChart from '@/components/home-page/kosdaq-kospi-chart/kosdaq-chart';
+
 export const KosdaqKospiChartContainer = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <MainKosdaqChart />
+      </div>
     </div>
   );
 };
--- src/routes/pages/HomePage.tsx
+++ src/routes/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useUserRanking } from '@/api/home.api';
+import { KosdaqKospiChartContainer } from '@/components/home-page/kosdaq-kospi-chart/kosdaq-kospi-chart-container';
 import { NewsChart } from '@/components/home-page/news/news-chart';
 import { RankCards } from '@/components/home-page/rank-cards/rank-cards';
 import { RealTimeChart } from '@/components/home-page/real-time-chart/real-time-chart';
@@ -11,6 +12,9 @@ export const HomePage = () => {
       <div>{/* 코스피,코스단.. 차트 */}</div>
       <div>
         <div className="mt-[100px] w-full">
+          <div>
+            <KosdaqKospiChartContainer />
+          </div>
           <div className="mb-[25px] flex items-center gap-3">
             <h3 className="text-[28px] font-bold">실시간 차트</h3>
             <p className="text-[20px] font-light text-text-inactive-2-color">어제 08:25 기준</p>
