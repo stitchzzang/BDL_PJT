61d976256ef87c53794ccc050f7bbffdac465d2a
frndhoon <frndhoon@gmail.com>
2025-04-02 00:40:52 +0900

    fix: react query dev tool 에러 임시 주석 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 
 import { queryClient } from '@/lib/queryClient';
@@ -10,7 +9,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <AppRouter />
       <Toaster position="bottom-center" />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 }
