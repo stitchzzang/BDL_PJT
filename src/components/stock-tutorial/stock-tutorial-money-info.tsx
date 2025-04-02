2b8543f142b436621728528c6623df284c652736
신해인 <godls0215@naver.com>
2025-04-02 22:22:49 +0900

    design: 금액 정보 공백 수정

--- src/components/stock-tutorial/stock-tutorial-money-info.tsx
+++ src/components/stock-tutorial/stock-tutorial-money-info.tsx
@@ -23,19 +23,19 @@ export const StockTutorialMoneyInfo = ({
   return (
     <div className="flex gap-3">
       <div className="flex gap-2 rounded-xl bg-modal-background-color px-[15px] py-[15px]">
-        <p className="text-border-color">시드머니:</p>
+        <p className="text-border-color">시드머니 :</p>
         <span className="font-bold">{addCommasToThousand(initialAsset)}원</span>
       </div>
       <div className="flex gap-2 rounded-xl bg-modal-background-color px-[15px] py-[15px]">
-        <p className="text-border-color">주문가능:</p>
+        <p className="text-border-color">주문가능 :</p>
         <span className="font-bold">{addCommasToThousand(availableOrderAsset)}원</span>
       </div>
       <div className="flex gap-2 rounded-xl bg-modal-background-color px-[15px] py-[15px]">
-        <p className="text-border-color">현재자산:</p>
+        <p className="text-border-color">현재자산 :</p>
         <span className="font-bold">{addCommasToThousand(currentTotalAsset)}원</span>
       </div>
       <div className={`flex gap-2 rounded-xl px-[15px] py-[15px] ${profitColor} bg-opacity-20`}>
-        <p className="text-border-color">총 수익률:</p>
+        <p className="text-border-color">총 수익률 :</p>
         <span className={`font-bold ${textProfitColor}`}>{displayPercentage}</span>
       </div>
     </div>
