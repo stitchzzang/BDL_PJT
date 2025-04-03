fbf19f928c03a39fa80f107d558ff72a87ffc17b
dnwnsrb11 <dnwnsrb11@naver.com>
2025-04-03 17:46:51 +0900

    feat:기본 그리드 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,6 +86,11 @@ export default {
       screens: {
         webapp: '860px',
       },
+      gridTemplateColumns: {
+        13: 'repeat(13, minmax(0, 1fr))',
+        14: 'repeat(14, minmax(0, 1fr))',
+        16: 'repeat(16, minmax(0, 1fr))',
+      },
     },
   },
   plugins: [tailwindAnimate],
