c4110c696c72b637c4dd9b153164e8407e3d9b03
frndhoon <frndhoon@gmail.com>
2025-04-03 17:27:07 +0900

    design: 튜토리얼 결과 페이지 현재 날짜 제거

--- src/routes/pages/member/TutorialResultPage.tsx
+++ src/routes/pages/member/TutorialResultPage.tsx
@@ -21,7 +21,6 @@ export const TutorialResultPage = () => {
     <div className="mx-auto flex w-full max-w-[1000px] flex-col items-center">
       <div className="flex w-full flex-row justify-between">
         <h1 className="text-2xl font-bold">주식 튜토리얼 결과</h1>
-        <p className="text-text-inactive-2-color">{new Date().toISOString().split('T')[0]}</p>
       </div>
       <hr className="my-3 w-full border-t border-btn-primary-inactive-color" />
       {isError ? (
