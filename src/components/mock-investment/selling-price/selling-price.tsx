8846b2c51491873d7f603541bae55a20baee91f2
WJG <dnwnsrb11@naver.com>
2025-04-04 04:59:31 +0900

    design:호가 디자인 변경

--- src/components/mock-investment/selling-price/selling-price.tsx
+++ src/components/mock-investment/selling-price/selling-price.tsx
@@ -1,10 +1,11 @@
+import Lottie from 'lottie-react';
 import { useEffect, useState } from 'react';
 
 import { CompanyInfo, OrderbookDatas } from '@/api/types/stock';
-import { ChartLoadingAnimation } from '@/components/common/chart-loading-animation';
+import walkMove from '@/assets/lottie/walk-animation.json';
 import { SellingPriceSell } from '@/components/mock-investment/selling-price/selling-price-sell';
+import { SparklesCore } from '@/components/ui/sparkles';
 import { useOrderbookConnection } from '@/services/SocketStockOrderbookDataService';
-
 interface SellingPrice {
   stockCompanyInfo?: CompanyInfo;
 }
@@ -24,9 +25,9 @@ export const SellingPrice = ({ stockCompanyInfo }: SellingPrice) => {
     };
   }, [connectOrderbook, disconnectOrderbook]);
   return (
-    <div className="h-full rounded-2xl bg-modal-background-color p-[20px]">
+    <>
       {orderbooks ? (
-        <>
+        <div className="h-full rounded-2xl bg-modal-background-color p-[20px]">
           <div className="flex justify-between">
             <h3 className="text-[18px] font-bold">호가</h3>
             {/* 장시간 string */}
@@ -40,13 +41,36 @@ export const SellingPrice = ({ stockCompanyInfo }: SellingPrice) => {
           <div>
             <SellingPriceSell orderbooks={orderbooks} />
           </div>
-        </>
+        </div>
       ) : (
-        <div className="flex flex-col items-center justify-center">
-          <ChartLoadingAnimation />
-          <p className="text-[16px] text-border-color">현재 장 시간이 아닙니다.</p>
+        <div className="relative h-full overflow-hidden rounded-2xl border border-border-color border-opacity-20 p-[20px]">
+          <div className="absolute inset-0 h-full w-full">
+            <SparklesCore
+              id="sparkles"
+              background="transparent"
+              minSize={0.6}
+              maxSize={1.4}
+              particleColor="#ffffff"
+              particleDensity={70}
+              className="h-full w-full"
+            />
+          </div>
+          <div className="flex items-center justify-center gap-4">
+            <Lottie
+              animationData={walkMove}
+              loop={true}
+              autoplay={true}
+              style={{ height: 150, width: 150 }}
+              rendererSettings={{
+                preserveAspectRatio: 'xMidYMid slice',
+              }}
+            />
+            <p className="text-[18px] text-border-color">
+              현재 <span className="font-bold text-btn-blue-color">거래시간</span>이 아닙니다.
+            </p>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
