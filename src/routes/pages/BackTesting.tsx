2cfcbf6b989c760339354a581b0b0181595d1acb
WJG <dnwnsrb11@naver.com>
2025-04-05 18:16:55 +0900

    feat:날짜 변경 로직 구현 - 클릭을 통하여 일자별 데이터를 보여주기 가능

--- src/routes/pages/BackTesting.tsx
+++ src/routes/pages/BackTesting.tsx
@@ -7,7 +7,12 @@ import { AlgorithmCompanyInfo } from '@/components/algorithm/algorithm-company-i
 
 export const BackTesting = () => {
   const [companyProfile, setCompanyProfile] = useState<CompanyProfile | null>(null);
+  // 출력 봉
   const [dailyData, setDailyData] = useState<StockDailyData[] | null>(null);
+  // 전체 봉 저장
+  const [saveDailyData, setSaveDailyData] = useState<StockDailyData[] | null>(null);
+  // 전체 봉 개수
+  const [dailyCount, setDailyCount] = useState<number>(0);
   const backTestAlgorithm = useBackTestAlgorithm();
   // 백테스팅 자료 요청
   const handleBackTest = () => {
@@ -22,10 +27,18 @@ export const BackTesting = () => {
         onSuccess: (res) => {
           setCompanyProfile(res.companyProfile);
           setDailyData(res.stockDaily.data);
+          setSaveDailyData(res.stockDaily.data);
+          setDailyCount(res.stockDaily.limit);
           console.log(res);
         },
       },
     );
+  };
+
+  const handlerDay = (now: number) => {
+    if (dailyData) {
+      setDailyData(dailyData.slice(0, now));
+    }
   };
   useEffect(() => {
     handleBackTest();
@@ -36,7 +49,13 @@ export const BackTesting = () => {
         <AlgorithmCompanyInfo companyProfile={companyProfile} />
       </div>
       <div>
+        <div>
+          <p>전체 일자 : {dailyCount}</p>
+        </div>
         <CandlestickAlgorithmChart data={dailyData} />
+      </div>
+      <div>
+        <button onClick={() => handlerDay(100)}>클릭</button>
       </div>
     </div>
   );
