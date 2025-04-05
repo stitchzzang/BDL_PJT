0d64f165b8c61f5d54df02b2691d25a9c94a8e90
WJG <dnwnsrb11@naver.com>
2025-04-05 16:47:59 +0900

    feat:api 연결

--- src/routes/pages/BackTesting.tsx
+++ src/routes/pages/BackTesting.tsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+
+import { useBackTestAlgorithm } from '@/api/algorithm.api';
+import { CompanyProfile } from '@/api/types/algorithm';
 import { AlgorithmCompanyInfo } from '@/components/algorithm/algorithm-company-info';
 
 export const BackTesting = () => {
+  const [companyProfile, setCompanyProfile] = useState<CompanyProfile | null>(null);
+  const backTestAlgorithm = useBackTestAlgorithm();
+  const handleBackTest = () => {
+    console.log('cool!!!');
+    backTestAlgorithm.mutate(
+      {
+        algorithmId: 20, // 실제 알고리즘 ID로 변경
+        companyId: 1, // 실제 회사 ID로 변경
+        startDate: '2024-01-01',
+        endDate: '2025-01-01',
+      },
+      {
+        onSuccess: (res) => {
+          setCompanyProfile(res.companyProfile);
+          console.log(res);
+        },
+      },
+    );
+  };
   return (
     <div className="w-full px-6">
       <div className="">
-        <AlgorithmCompanyInfo />
+        <AlgorithmCompanyInfo companyProfile={companyProfile} />
+      </div>
+      <div>
+        <button onClick={() => handleBackTest()}>test</button>
       </div>
     </div>
   );
