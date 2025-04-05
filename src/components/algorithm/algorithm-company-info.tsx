8ec1432c671b46807ec10ccb5cc2637509990d71
WJG <dnwnsrb11@naver.com>
2025-04-05 17:10:11 +0900

    feat:회사 정보 전달 porps 설정

--- src/components/algorithm/algorithm-company-info.tsx
+++ src/components/algorithm/algorithm-company-info.tsx
@@ -1,9 +1,13 @@
-export const AlgorithmCompanyInfo = () => {
+import { CompanyProfile } from '@/api/types/algorithm';
+
+interface AlgorithmCompanyInfoProps {
+  companyProfile: CompanyProfile | null;
+}
+
+export const AlgorithmCompanyInfo = ({ companyProfile }: AlgorithmCompanyInfoProps) => {
   return (
     <div className="border">
-      <div>
-        <p>hello</p>
-      </div>
+      <div>{companyProfile ? <p>{companyProfile.companyName}</p> : <p>no Data</p>}</div>
     </div>
   );
 };
