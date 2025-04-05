496b98a7987f772d3fdd62f2ac20b8af3e8f0523
frndhoon <frndhoon@gmail.com>
2025-04-05 18:40:26 +0900

    design: 투자 스타일 페이지의 이익률 및 손절매 설명을 아이콘과 함께 시각적으로 개선

--- src/routes/pages/algorithm-lab/StylePage.tsx
+++ src/routes/pages/algorithm-lab/StylePage.tsx
@@ -1,3 +1,4 @@
+import { ArrowTrendingDownIcon, ArrowTrendingUpIcon } from '@heroicons/react/24/solid';
 import Lottie from 'lottie-react';
 import { useNavigate } from 'react-router-dom';
 
@@ -74,25 +75,56 @@ export const StylePage = () => {
         description="3가지 선택지 중 가장 선호하는 투자 스타일을 선택하거나 바를 드래그하여 원하는 값을 선택해주세요."
       />
 
-      <div className="w-full rounded-2xl bg-gray-50 p-4 text-sm shadow-sm">
-        <div className="mb-3">
-          <p className="text-lg font-semibold text-primary-color">이익률이란?</p>
-          <p className="whitespace-pre-line break-keep text-gray-600">
-            주식 가격이 얼마나 올라갔을 때 팔지를 결정하는 비율입니다. <br />
-            예를 들어, 이익률이 10%라면 주식을 산 가격보다 10% 더 올라갔을 때 자동으로 팔게 됩니다.
-            <br />
-            이익률이 높을수록 더 큰 수익을 기대할 수 있지만, 가격이 떨어질 위험도 있습니다. 수익을
-            기대할 수 있지만, 가격이 떨어질 위험도 있습니다.
-          </p>
-        </div>
+      <div className="w-full rounded-2xl bg-gray-50 p-4 shadow-sm">
         <div>
-          <p className="text-lg font-semibold text-primary-color">손절매란?</p>
-          <p className="whitespace-pre-line break-keep text-gray-600">
-            주식 가격이 얼마나 내려갔을 때 손해를 감수하고 팔지를 결정하는 비율입니다. <br />
-            예를 들어, 손절매가 5%라면 주식을 산 가격보다 5% 더 내려갔을 때 자동으로 팔게 됩니다.
-            <br />
-            이는 더 큰 손실을 방지하기 위한 안전장치입니다.
+          <p className="flex items-center text-lg font-bold text-primary-color">
+            <ArrowTrendingUpIcon className="mr-1 h-5 w-5" />
+            이익률이란?
           </p>
+          <p className="text-sm text-gray-600">
+            주식 가격이 <b className="text-primary-color">얼마나 올라갔을 때 팔지</b>를 결정하는
+            비율입니다.
+          </p>
+          <div className="my-2 rounded-lg bg-white p-2">
+            <p className="text-sm text-gray-700">
+              예를 들어, 이익률이 10%라면 주식을 산 가격보다
+              <br />
+              <b className="text-primary-color">10% 더 올라갔을 때 자동으로 팔게</b> 됩니다.
+            </p>
+          </div>
+          <div className="rounded-lg bg-white p-2">
+            <p className="text-sm text-gray-700">
+              이익률이 높을수록 <b className="text-primary-color">더 큰 수익을 기대</b>할 수 있지만,
+              <br />
+              <b className="text-primary-color">가격이 떨어질 위험</b>도 있습니다.
+            </p>
+          </div>
+        </div>
+
+        <hr className="my-4 border-t border-gray-300" />
+
+        <div>
+          <p className="flex items-center text-lg font-bold text-primary-color">
+            <ArrowTrendingDownIcon className="mr-1 h-5 w-5" />
+            손절매란?
+          </p>
+          <p className="text-sm text-gray-600">
+            주식 가격이{' '}
+            <b className="text-primary-color">얼마나 내려갔을 때 손해를 감수하고 팔지</b>를<br />
+            결정하는 비율입니다.
+          </p>
+          <div className="my-2 rounded-lg bg-white p-2">
+            <p className="text-sm text-gray-700">
+              예를 들어, 손절매가 5%라면 주식을 산 가격보다
+              <br />
+              <b className="text-primary-color">5% 더 내려갔을 때 자동으로 팔게</b> 됩니다.
+            </p>
+          </div>
+          <div className="rounded-lg bg-white p-2">
+            <p className="text-sm text-gray-700">
+              이는 <b className="text-primary-color">더 큰 손실을 방지하기 위한 안전장치</b>입니다.
+            </p>
+          </div>
         </div>
       </div>
 
