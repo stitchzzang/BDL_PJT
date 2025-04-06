855b4929e974f7c048550b45c026fb1e094eb0b0
WJG <dnwnsrb11@naver.com>
2025-04-06 21:31:27 +0900

    feat:기초 정보 전달 구조화 - 결과창 컴포넌트

--- src/components/algorithm/algorithm-summary.tsx
+++ src/components/algorithm/algorithm-summary.tsx
@@ -1,7 +1,29 @@
-export const AlgorithmSummary = () => {
+import { Summary } from '@/api/types/algorithm';
+
+interface AlgorithmSummaryProps {
+  summary: Summary;
+}
+
+export const AlgorithmSummary = ({ summary }: AlgorithmSummaryProps) => {
   return (
     <div>
-      <div></div>
+      <div>
+        <p>수익률 : {summary.rateOfReturn}</p>
+        <p>수익률 : {summary.totalTrades}</p>
+        <p>수익률 : {summary.startingCapital}</p>
+        <p>수익률 : {summary.endingCapital}</p>
+        <p>수익률 : {summary.maxDrawdown}</p>
+        <p>수익률 : {summary.winRate}</p>
+      </div>
     </div>
   );
 };
+
+// 백테스트 결과 요약 정보를 제공합니다.
+
+// - **rateOfReturn**: 수익률(%)
+// - **totalTrades**: 총 거래 횟수
+// - **startingCapital**: 초기 자본금
+// - **endingCapital**: 최종 자본금
+// - **maxDrawdown**: 최대 낙폭(%)
+// - **winRate**: 승률(%)
