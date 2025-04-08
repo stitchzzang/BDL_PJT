c6c257b91167d2a38ebce21161eba9fec72e3bca
frndhoon <frndhoon@gmail.com>
2025-04-08 10:30:27 +0900

    feat: 랭크 카드 에러일 때, errorScreen ui 추가

--- src/components/home-page/rank-cards/rank-cards.tsx
+++ src/components/home-page/rank-cards/rank-cards.tsx
@@ -1,4 +1,5 @@
 import { UserRanking } from '@/api/types/home';
+import { ErrorScreen } from '@/components/common/error-screen';
 import { RankCard } from '@/components/home-page/rank-cards/rank-card';
 import { LoadingSpinner } from '@/components/ui/loading-spinner';
 
@@ -12,13 +13,15 @@ export const RankCards = ({
   userRanking,
   isLoading,
   isError,
+  refetch,
 }: {
   userRanking: UserRanking[];
   isLoading: boolean;
   isError: boolean;
+  refetch: () => void;
 }) => {
   if (isLoading) return <LoadingSpinner />;
-  if (isError) return <div>에러가 발생했습니다.</div>;
+  if (isError) return <ErrorScreen onRefresh={refetch} />;
   if (userRanking.length === 0) return <div>랭킹 데이터가 없습니다.</div>;
   return (
     <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
