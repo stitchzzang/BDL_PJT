1c770c4edf2ed0220cbfe6e1f3362b1722bdcfe1
frndhoon <frndhoon@gmail.com>
2025-04-10 11:59:16 +0900

    design: URI TOO LARGE 메시지 수정

--- src/routes/pages/TooLargePage.tsx
+++ src/routes/pages/TooLargePage.tsx
@@ -13,7 +13,7 @@ export const TooLargePage = () => {
       <div className="flex flex-col items-center justify-center gap-6">
         <h1 className="text-5xl font-bold text-primary-color">414</h1>
         <div className="flex flex-col items-center gap-2">
-          <p className="text-3xl font-medium">TOO LARGE</p>
+          <p className="text-3xl font-medium">URI TOO LARGE</p>
         </div>
         <Button variant="blue" onClick={() => navigate('/')} className="mt-4 px-8 py-6 text-lg">
           홈으로 돌아가기
